81c92fb2aee7559057d31578e2c7d7dbc201065f
Sandro Munda <sandro@munda.me>
2016-05-11 14:20:20 +0200

    Fix the liana name/version

--- index.js
+++ index.js
@@ -254,8 +254,8 @@ exports.init = function (Implementation) {
             attributes: ['name', 'endpoint', 'httpMethod']
           },
           meta: {
-            'liana': 'forest-express-mongoose',
-            'liana_version': require('./package.json').version
+            'liana': Implementation.getLianaName(),
+            'liana_version': Implementation.getLianaVersion()
           }
         });
 
