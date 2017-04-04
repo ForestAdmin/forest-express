86066ba8f5c28f65992c61236f5346858fb86d57
Louis-Rémi Babé <lrbabe@gmail.com>
2017-04-04 12:10:40 +0200

    Use async bcrypt, which is more efficient

--- routes/sessions.js
+++ routes/sessions.js
@@ -12,56 +12,61 @@ module.exports = function (app, opts) {
     new AllowedUsersFinder(request.body.renderingId, opts)
       .perform()
       .then(function (allowedUsers) {
+        if (!opts.authSecret) {
+          throw new Error('Your Forest authSecret seems to be missing. Can ' +
+            'you check that you properly set a Forest authSecret in the ' +
+            'Forest initializer?');
+        }
+
         if (allowedUsers.length === 0) {
-          return response.status(401).send({
-            errors: [{
-              detail: 'Forest cannot retrieve any users for the project ' +
-                'you\'re trying to unlock.'
-            }]
-          });
+          throw new Error('Forest cannot retrieve any users for the project ' +
+            'you\'re trying to unlock.');
         }
 
         var user = _.find(allowedUsers, function (allowedUser) {
-          return allowedUser.email === request.body.email &&
-            bcrypt.compareSync(request.body.password, allowedUser.password);
+          return allowedUser.email === request.body.email;
         });
 
-        if (user) {
-          if (opts.authSecret) {
-            var token = jwt.sign({
-              id: user.id,
-              type: 'users',
-              data: {
-                email: user.email,
-                'first_name': user['first_name'],
-                'last_name': user['last_name'],
-                teams: user.teams
-              },
-              relationships: {
-                renderings: {
-                  data: [{
-                    type: 'renderings',
-                    id: request.body.renderingId
-                  }]
-                }
-              }
-            }, opts.authSecret, {
-              expiresIn: '14 days'
-            });
-
-            response.send({ token: token });
-          } else {
-            return response.status(401).send({
-              errors: [{
-                detail: 'Your Forest authSecret seems to be missing. Can you ' +
-                  'check that you properly set a Forest authSecret in the ' +
-                  'Forest initializer?'
-              }]
-            });
-          }
-        } else {
-          response.status(401).send();
+        if (user === undefined) {
+          throw new Error();
         }
+
+        return bcrypt.compare(request.body.password, user.password)
+          .then(function (isEqual) {
+            if (!isEqual) {
+              throw new Error();
+            }
+            return user;
+          });
+      })
+      .then(function (user) {
+        var token = jwt.sign({
+          id: user.id,
+          type: 'users',
+          data: {
+            email: user.email,
+            'first_name': user['first_name'],
+            'last_name': user['last_name'],
+            teams: user.teams
+          },
+          relationships: {
+            renderings: {
+              data: [{
+                type: 'renderings',
+                id: request.body.renderingId
+              }]
+            }
+          }
+        }, opts.authSecret, {
+          expiresIn: '14 days'
+        });
+
+        response.send({ token: token });
+      })
+      .catch(function (error) {
+        return response.status(401).send({
+          errors: [{ detail: error.message }]
+        });
       });
   }
 
