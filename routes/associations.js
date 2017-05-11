4bd41cdc8994d3bcb998cd07c9e558d43098c3ce
Lucas Scariot <lucas@forestadmin.com>
2017-05-11 12:09:36 +0200

    [*] Fix send smart fields in has many request

--- routes/associations.js
+++ routes/associations.js
@@ -1,5 +1,7 @@
 'use strict';
 var _ = require('lodash');
+var P = require('bluebird');
+var logger = require('../services/logger');
 var ResourceSerializer = require('../serializers/resource');
 var Schemas = require('../generators/schemas');
 var auth = require('../services/auth');
@@ -7,13 +9,55 @@ var path = require('../services/path');
 
 module.exports = function (app, model, Implementation, integrator, opts) {
   var modelName = Implementation.getModelName(model);
+  var schema = Schemas.schemas[modelName];
+  var schemaAssociation;
+
 
   function getAssociationField(associationName) {
-    var schema = Schemas.schemas[Implementation.getModelName(model)];
     var field = _.find(schema.fields, { field: associationName });
     if (field && field.reference) {
       return field.reference.split('.')[0];
     }
+  }
+
+  function setSmartFieldValue(record, field, modelName) {
+    if (field.value) {
+      logger.warn('DEPRECATION WARNING: Smart Fields "value" method is ' +
+        'deprecated. Please use "get" method in your collection ' +
+        modelName + ' instead.');
+    }
+
+    var value = field.get ? field.get(record) : field.value(record);
+    if (value && _.isFunction(value.then)) {
+      return value.then(function (result) { record[field.field] = result; });
+    } else {
+      record[field.field] = value;
+    }
+  }
+
+  function injectSmartFields(record) {
+    return P.each(schemaAssociation.fields, function (field) {
+      if (!record[field.field]) {
+        if (field.get || field.value) {
+          return setSmartFieldValue(record, field, modelName);
+        } else if (_.isArray(field.type)) {
+          record[field.field] = [];
+        }
+      } else if (field.reference) {
+        var modelNameAssociation = field.reference.split('.')[0];
+        var schemaAssociation = Schemas.schemas[modelNameAssociation];
+
+        if (schemaAssociation) {
+          return P.each(schemaAssociation.fields, function (fieldAssociation) {
+            if (!record[field.field][fieldAssociation.field] &&
+              (fieldAssociation.get || fieldAssociation.value)) {
+              return setSmartFieldValue(record[field.field],
+                fieldAssociation, modelNameAssociation);
+            }
+          });
+        }
+      }
+    }).thenReturn(record);
   }
 
   function index(request, response, next) {
@@ -23,6 +67,7 @@ module.exports = function (app, model, Implementation, integrator, opts) {
     var associationModel = _.find(models, function (model) {
       return Implementation.getModelName(model) === associationField;
     });
+    schemaAssociation = Schemas.schemas[associationModel.name];
 
     if (!associationModel) { return response.status(404).send(); }
 
@@ -33,8 +78,12 @@ module.exports = function (app, model, Implementation, integrator, opts) {
         var count = results[0];
         var records = results[1];
 
-        return new ResourceSerializer(Implementation, associationModel,
-          records, integrator, opts, { count: count }).perform();
+        return P
+          .map(records, injectSmartFields)
+          .then(function (records) {
+            return new ResourceSerializer(Implementation, associationModel,
+              records, integrator, opts, { count: count }).perform();
+          });
       })
       .then(function (records) { response.send(records); })
       .catch(next);
