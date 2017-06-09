54b37484613dbf698e9922810ff6acef636b5096
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-06-09 10:41:15 +0200

    [-] Error Messages - Display the stack trace on unexpected errors

--- services/allowed-users-finder.js
+++ services/allowed-users-finder.js
@@ -24,15 +24,17 @@ function AllowedUsersFinder(renderingId, opts) {
             });
           } else {
             if (result.status === 0) {
-              logger.error('Cannot retrieve any users for the project you\'re ' +
-                'trying to unlock. Forest API seems to be down right now.');
+              logger.error('Cannot retrieve any users for the project ' +
+                'you\'re trying to unlock. Forest API seems to be down right ' +
+                'now.');
             } else if (result.status === 404) {
               logger.error('Cannot retrieve the project you\'re trying to ' +
                 'unlock. Can you check that you properly copied the Forest ' +
                 'envSecret in the forest_liana initializer?');
             } else {
               logger.error('Cannot retrieve any users for the project ' +
-                'you\'re trying to unlock. An error occured in Forest API.');
+                'you\'re trying to unlock. An error occured in Forest API.',
+                error);
             }
           }
           resolve(allowedUsers);
