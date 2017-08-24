e769bb2f6b81e19144a9783efadc383264833ef2
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-08-24 12:31:32 +0200

    [*] Code Inspection - Fix Forest customisation code inspection to be recursive through directories

--- utils/code-syntax-inspector.js
+++ utils/code-syntax-inspector.js
@@ -1,34 +1,44 @@
 'use strict';
 var P = require('bluebird');
 var esprima = require('esprima');
-var path = require('path');
 var fs = require('fs');
 var Pfs = P.promisifyAll(fs);
 var logger = require('../services/logger');
 
-exports.extractCodeSyntaxErrorInDirectoryFile = function (modelsDir) {
+exports.extractCodeSyntaxErrorInDirectoryFile = function (directory) {
   var hasError = false;
+  var listFiles = [];
 
-  if (fs.existsSync(modelsDir)) {
-    return Pfs.readdirAsync(modelsDir)
-      .map(function (filename) {
-        var file = path.join(modelsDir, filename);
-        var fileContent = Pfs.readFileSync(file);
-
-        try {
-          return esprima.parse(fileContent.toString(), { loc: true });
-        } catch (error) {
-          if (error) {
-            hasError = true;
-            logger.error('Forest customization failed due to a syntax error: ' +
-              error.description + ' in ' + file + ':' + error.lineNumber);
-          }
-        }
-      })
-      .then(function () { return hasError; });
-  } else {
-    return new P(function(resolve) {
-      return resolve(hasError);
+  function getDirectoryFiles(parentDirectory) {
+    var files = fs.readdirSync(parentDirectory);
+    files.forEach(function (file) {
+      var path = parentDirectory + '/' + file;
+      if (fs.statSync(path).isDirectory()){
+        getDirectoryFiles(path);
+      } else {
+        listFiles.push(path);
+      }
     });
+  }
+
+  if (fs.existsSync(directory)) {
+    getDirectoryFiles(directory);
+    listFiles.forEach(function (file) {
+      var fileContent = Pfs.readFileSync(file);
+
+      try {
+        return esprima.parse(fileContent.toString(), { loc: true });
+      } catch (error) {
+        if (error) {
+          hasError = true;
+          logger.error('Forest customization failed due to a syntax error: ' +
+            error.description + ' in ' + file + ':' + error.lineNumber);
+        }
+      }
+    });
+
+    return new P(function(resolve) { return resolve(hasError); });
+  } else {
+    return new P(function(resolve) { return resolve(hasError); });
   }
 };
