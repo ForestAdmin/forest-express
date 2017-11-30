fef81d16142d38c0430388cd1d1934793127d952
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-11-30 15:31:51 +0100

    [-] Code style - Fix some indentation

--- index.js
+++ index.js
@@ -220,10 +220,10 @@ exports.init = function (Implementation) {
             if (collection.actions) {
               _.each(collection.actions, function (action) {
                 if (action.fields && !_.isArray(action.fields)) {
-                   logger.error('Cannot find the fields you defined for the ' +
-                     'Smart action "' + action.name + '" of your "' +
-                     collection.name + '" collection. The fields option must ' +
-                     'be an array.');
+                  logger.error('Cannot find the fields you defined for the ' +
+                    'Smart action "' + action.name + '" of your "' +
+                    collection.name + '" collection. The fields option must ' +
+                    'be an array.');
                 }
               });
             }
@@ -266,33 +266,33 @@ exports.init = function (Implementation) {
 
           request
             .post(forestUrl + '/forest/apimaps')
-              .send(apimap)
-              .set('forest-secret-key', opts.envSecret)
-              .end(function (error, result) {
-                if (result) {
-                  if ([200, 202, 204].indexOf(result.status) !== -1) {
-                    if (result.body && result.body.warning) {
-                      logger.warn(result.body.warning);
-                    }
-                  } else if (result.status === 0) {
-                    logger.warn('Cannot send the apimap to Forest. Are you ' +
-                      'online?');
-                  } else if (result.status === 404) {
-                    logger.error('Cannot find the project related to the ' +
-                      'envSecret you configured. Can you check on Forest ' +
-                      'that you copied it properly in the Forest ' +
-                      'initialization?');
-                  } else if (result.status === 503) {
-                    logger.warn('Forest is in maintenance for a few minutes. ' +
-                      'We are upgrading your experience in the forest. We ' +
-                      'just need a few more minutes to get it right.');
-                  } else {
-                    logger.error('An error occured with the apimap sent to ' +
-                      'Forest. Please contact support@forestadmin.com for ' +
-                      'further investigations.');
+            .send(apimap)
+            .set('forest-secret-key', opts.envSecret)
+            .end(function (error, result) {
+              if (result) {
+                if ([200, 202, 204].indexOf(result.status) !== -1) {
+                  if (result.body && result.body.warning) {
+                    logger.warn(result.body.warning);
                   }
+                } else if (result.status === 0) {
+                  logger.warn('Cannot send the apimap to Forest. Are you ' +
+                    'online?');
+                } else if (result.status === 404) {
+                  logger.error('Cannot find the project related to the ' +
+                    'envSecret you configured. Can you check on Forest ' +
+                    'that you copied it properly in the Forest ' +
+                    'initialization?');
+                } else if (result.status === 503) {
+                  logger.warn('Forest is in maintenance for a few minutes. ' +
+                    'We are upgrading your experience in the forest. We ' +
+                    'just need a few more minutes to get it right.');
+                } else {
+                  logger.error('An error occured with the apimap sent to ' +
+                    'Forest. Please contact support@forestadmin.com for ' +
+                    'further investigations.');
                 }
-              });
+              }
+            });
         }
       }
     })
