9ae4aa964789c299aa31507fc8d1edda5113d6a9
Arnaud Valensi <arnaud.valensi@gmail.com>
2017-12-12 20:47:23 +0100

    add mocha with babel

---
+++ test/routes/sessions-google.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('API > Google OAuth2 SignIn/Login', () => {
+  describe('GET /forest/sessions-google', () => {
+    it('should pass', () => {
+      expect(1).equal(1);
+    });
+  });
+});
