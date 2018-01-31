a9ed4afe8262a77f27dc0a50d7973530e10331da
Lucas Scariot <lucas@scariot.fr>
2018-01-31 16:49:24 +0100

    Naming

--- services/apimap-sorter.js
+++ services/apimap-sorter.js
@@ -1,7 +1,7 @@
 'use strict';
 var _ = require('lodash');
 
-function order(object) {
+function sortObjectKeys(object) {
   var sortedObject = {};
 
   _.each(_.sortBy(Object.keys(object)), function(key) {
@@ -15,7 +15,7 @@ function ApimapSorter(apimap) {
   apimap.data = _.sortBy(apimap.data, ['type', 'id']);
 
   _.each(apimap.data, function (data) {
-    data.attributes = order(data.attributes);
+    data.attributes = sortObjectKeys(data.attributes);
     data.fields = _.sortBy(apimap.fields, ['field', 'type']);
   });
 
@@ -23,11 +23,11 @@ function ApimapSorter(apimap) {
     apimap.included = _.sortBy(apimap.included, ['type', 'id']);
 
     _.each(apimap.included, function (include) {
-      include.attributes = order(include.attributes);
+      include.attributes = sortObjectKeys(include.attributes);
     });
   }
 
-  apimap.meta = order(apimap.meta);
+  apimap.meta = sortObjectKeys(apimap.meta);
 
   return apimap;
 }
