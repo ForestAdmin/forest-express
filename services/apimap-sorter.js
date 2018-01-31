10b4a424fcb60e5953588537beaf7ea9c7e6b0ad
Lucas Scariot <lucas@scariot.fr>
2018-01-31 15:40:55 +0100

    Update sorting with object keys

--- services/apimap-sorter.js
+++ services/apimap-sorter.js
@@ -1,16 +1,29 @@
 'use strict';
 var _ = require('lodash');
 
+function order(object) {
+  var sortedObject = {};
+
+  _.each(_.sortBy(Object.keys(object)), function(key) {
+    sortedObject[key] = object[key];
+  });
+
+  return sortedObject;
+}
+
 function ApimapSorter(apimap) {
   apimap.data = _.sortBy(apimap.data, ['type', 'id']);
 
   _.each(apimap.data, function (data) {
+    data.attributes = order(data.attributes);
     data.fields = _.sortBy(apimap.fields, ['field', 'type']);
   });
 
   if (apimap.included) {
     apimap.included = _.sortBy(apimap.included, ['type', 'id']);
   }
+
+  apimap.meta = order(apimap.meta);
 
   return apimap;
 }
