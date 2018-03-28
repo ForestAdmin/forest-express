60ea5014695328364dd6ec989b28a98308e0fd96
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-03-28 11:12:24 +0200

    [+] Premium Security - Users that won't use an authorized IP won't have access to the data

---
+++ test/helpers/request.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const nock = require('nock');
+const ServiceUrlGetter = require('../../services/service-url-getter');
+
+const urlService = new ServiceUrlGetter().perform();
+
+nock(urlService)
+  .persist()
+  .get('/liana/ip-whitelist-rules')
+  .reply(200, {
+    data: {
+      type: 'ip-whitelist-rules',
+      id: '1',
+      attributes: {
+        use_ip_whitelist: false,
+        rules: [],
+      },
+    },
+  });
+
+module.exports = request;
--- utils/error-messages.js
+++ utils/error-messages.js
@@ -3,6 +3,7 @@
 exports.SESSION = {
   NO_USERS: 'Forest cannot retrieve any users for the project you\'re trying to unlock.',
   SECRET_AND_RENDERINGID_INCONSISTENT: 'Cannot retrieve the project you\'re trying to unlock. The envSecret and renderingId seems to be missing or inconsistent.',
+  SECRET_AND_PROJECTID_INCONSISTENT: 'Cannot retrieve the project you\'re trying to unlock. The envSecret and projectId seems to be missing or inconsistent.',
   SECRET_NOT_FOUND: 'Cannot retrieve the project you\'re trying to unlock. Can you check that you properly copied the Forest envSecret in the Liana initializer?',
   SERVER_DOWN: 'Cannot retrieve any users for the project you\'re trying to unlock. Forest API seems to be down right now.',
   UNEXPECTED: 'Cannot retrieve the user for the project you\'re trying to unlock. An error occured in Forest API.',
@@ -10,4 +11,10 @@ exports.SESSION = {
 
 exports.CONFIGURATION = {
   AUTH_SECRET_MISSING: 'Your Forest authSecret seems to be missing. Can you check that you properly set a Forest authSecret in the Forest initializer?',
+};
+
+exports.IP_WHITELIST = {
+  SERVER_DOWN: 'Cannot retrieve the list of whitelisted IPs. Forest API seems to be down right now.',
+  SECRET_NOT_FOUND: 'Cannot retrieve the list of whitelisted IPs. Can you check that you properly copied the Forest envSecret in the Liana initializer?',
+  UNEXPECTED: 'Cannot retrieve the list of whitelisted IPs. An error occured in Forest API.',
 };
