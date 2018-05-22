b681a8c2c821da68c52acdaa4e142d5fdbcdea6f
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-05-22 18:28:21 +0200

    Add expiration

--- src/services/permissions-checker.js
+++ src/services/permissions-checker.js
@@ -1,7 +1,10 @@
 const P = require('bluebird');
 const ForestServerRequester = require('./forest-server-requester');
+const moment = require('moment');
 
+const EXPIRATION_IN_SECONDS = process.env.PREMISSIONS_EXPIRATION || 3600;
 let permissions;
+let lastRetrieve;
 
 function PermissionChecker(environmentSecret, collectionName, permissionName) {
   function isAllowed() {
@@ -23,14 +26,34 @@ function PermissionChecker(environmentSecret, collectionName, permissionName) {
       });
   }
 
+  function isPermissionExpired() {
+    const currentTime = moment();
+
+    if (!lastRetrieve) {
+      return true;
+    }
+
+    const elapsedSeconds = currentTime.diff(lastRetrieve, 'seconds');
+
+    return elapsedSeconds >= EXPIRATION_IN_SECONDS;
+  }
+
+  function retrievePermissionsAndCheckAllowed(resolve, reject) {
+    return retrievePermissions()
+      .then(() => isAllowed()
+        ? resolve()
+        : reject(new Error(`'${permissionName}' access forbidden on ${collectionName}`)))
+      .catch(reject);
+  }
+
   this.perform = () => {
     return new P((resolve, reject) => {
+      if (isPermissionExpired()) {
+        return retrievePermissionsAndCheckAllowed(resolve, reject);
+      }
+
       if (!isAllowed(collectionName, permissionName)) {
-        return retrievePermissions()
-          .then(() => isAllowed()
-            ? resolve()
-            : reject(new Error(`'${permissionName}' access forbidden on ${collectionName}`)))
-          .catch(reject);
+        return retrievePermissionsAndCheckAllowed(resolve, reject);
       }
 
       return resolve();
