6ac04a9626a59ba1a8114c948c261f54a06fa648
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-05-23 11:27:51 +0200

    Improve tests describe

--- test/services/permissions.js
+++ test/services/permissions.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const PermissionsChecker= require('../../src/services/permissions-checker');
 const nock = require('nock');
 const ServiceUrlGetter = require('../../src/services/service-url-getter');
-const moment = require('moment');
 
 const { expect } = chai;
 
@@ -76,86 +75,151 @@ describe('Service > Permissions', () => {
     });
   });
 
-  describe('Check permissions expiration', () => {
+  describe('Check expiration', () => {
     beforeEach(() => {
       PermissionsChecker.resetExpiration();
       PermissionsChecker.cleanCache();
       nock.cleanAll();
     });
 
-    it('should retrieve the permissions if last retrieve is null', (done) => {
-      let lastRetrieve = PermissionsChecker.getLastRetrieveTime();
-      let retrievedPermissions = PermissionsChecker.getPermissions();
+    describe('with permissions never retrieved', () => {
+      it('should retrieve the permissions', (done) => {
+        let lastRetrieve = PermissionsChecker.getLastRetrieveTime();
+        let retrievedPermissions = PermissionsChecker.getPermissions();
 
-      expect(lastRetrieve).to.be.null;
-      expect(retrievedPermissions).to.be.null;
+        expect(lastRetrieve).to.be.null;
+        expect(retrievedPermissions).to.be.null;
 
-      const permissions = {
-        Users: {
-          permissions: {
-            list: true,
+        const permissions = {
+          Users: {
+            permissions: {
+              list: true,
+            },
           },
-        },
-      };
+        };
 
-      nockObj.get('/liana/v1/permissions').reply(200, permissions);
+        nockObj.get('/liana/v1/permissions').reply(200, permissions);
 
-      new PermissionsChecker('envSecret', 'Users', 'list')
-        .perform()
-        .then(() => {
-          retrievedPermissions = PermissionsChecker.getPermissions();
-          lastRetrieve = PermissionsChecker.getLastRetrieveTime();
+        new PermissionsChecker('envSecret', 'Users', 'list')
+          .perform()
+          .then(() => {
+            retrievedPermissions = PermissionsChecker.getPermissions();
+            lastRetrieve = PermissionsChecker.getLastRetrieveTime();
 
-          expect(lastRetrieve).to.not.be.null;
-          expect(retrievedPermissions).to.deep.equal(permissions);
+            expect(lastRetrieve).to.not.be.null;
+            expect(retrievedPermissions).to.deep.equal(permissions);
 
-          done();
-        })
-        .catch(done);
+            done();
+          })
+          .catch(done);
+      });
     });
 
-    it('should re-retrieve the permissions if it is expired', (done) => {
-      process.env.PERMISSIONS_EXPIRATION_IN_SECONDS = 1;
+    describe('with permissions expired', () => {
+      it('should re-retrieve the permissions', (done) => {
+        process.env.PERMISSIONS_EXPIRATION_IN_SECONDS = 1;
 
-      const intialLastRetrieve = PermissionsChecker.getLastRetrieveTime();
-      const initialRetrievedPermissions = PermissionsChecker.getPermissions();
+        const intialLastRetrieve = PermissionsChecker.getLastRetrieveTime();
+        const initialRetrievedPermissions = PermissionsChecker.getPermissions();
 
-      expect(intialLastRetrieve).to.be.null;
-      expect(initialRetrievedPermissions).to.be.null;
+        expect(intialLastRetrieve).to.be.null;
+        expect(initialRetrievedPermissions).to.be.null;
 
-      const permissions1 = {
-        Users: {
-          permissions: {
-            list: true,
+        const permissions1 = {
+          Users: {
+            permissions: {
+              list: true,
+            },
           },
-        },
-      };
+        };
 
-      const permissions2 = {
-        Users: {
-          permissions: {
-            list: true,
+        const permissions2 = {
+          Users: {
+            permissions: {
+              list: true,
+            },
           },
-        },
-        Posts: {
-          permissions: {
-            list: false,
+          Posts: {
+            permissions: {
+              list: false,
+            }
           }
-        }
-      };
+        };
 
-      nockObj.get('/liana/v1/permissions').reply(200, permissions1);
+        nockObj.get('/liana/v1/permissions').reply(200, permissions1);
 
-      new PermissionsChecker('envSecret', 'Users', 'list')
-        .perform()
-        .then(() => {
-          const firestRetrievedPermissions = PermissionsChecker.getPermissions();
-          const firstLastRetrieve = PermissionsChecker.getLastRetrieveTime();
+        new PermissionsChecker('envSecret', 'Users', 'list')
+          .perform()
+          .then(() => {
+            const firestRetrievedPermissions = PermissionsChecker.getPermissions();
+            const firstLastRetrieve = PermissionsChecker.getLastRetrieveTime();
 
-          expect(firestRetrievedPermissions).to.deep.equal(permissions1);
-          expect(firstLastRetrieve).to.be.not.null;
+            expect(firestRetrievedPermissions).to.deep.equal(permissions1);
+            expect(firstLastRetrieve).to.be.not.null;
 
-          setTimeout(() => {
+            setTimeout(() => {
+              nockObj.get('/liana/v1/permissions').reply(200, permissions2);
+
+              new PermissionsChecker('envSecret', 'Users', 'list')
+                .perform()
+                .then(() => {
+                  const secondRetrievedPermissions = PermissionsChecker.getPermissions();
+                  const secondLastRetrieve = PermissionsChecker.getLastRetrieveTime();
+
+                  expect(secondRetrievedPermissions).to.deep.equal(permissions2);
+                  expect(secondLastRetrieve.valueOf() - firstLastRetrieve.valueOf() > 0).to.be.true;
+
+                  done();
+                })
+                .catch(done);
+            }, 1200);
+          })
+          .catch(done);
+      });
+    });
+
+    describe('with permissions not expired', () => {
+      it('should not re-retrieve the permissions', (done) => {
+        process.env.PERMISSIONS_EXPIRATION_IN_SECONDS = 1000;
+
+        const intialLastRetrieve = PermissionsChecker.getLastRetrieveTime();
+        const initialRetrievedPermissions = PermissionsChecker.getPermissions();
+
+        expect(intialLastRetrieve).to.be.null;
+        expect(initialRetrievedPermissions).to.be.null;
+
+        const permissions1 = {
+          Users: {
+            permissions: {
+              list: true,
+            },
+          },
+        };
+
+        const permissions2 = {
+          Users: {
+            permissions: {
+              list: true,
+            },
+          },
+          Posts: {
+            permissions: {
+              list: false,
+            }
+          }
+        };
+
+        nockObj.get('/liana/v1/permissions').reply(200, permissions1);
+
+        new PermissionsChecker('envSecret', 'Users', 'list')
+          .perform()
+          .then(() => {
+            const firestRetrievedPermissions = PermissionsChecker.getPermissions();
+            const firstLastRetrieve = PermissionsChecker.getLastRetrieveTime();
+
+            expect(firestRetrievedPermissions).to.deep.equal(permissions1);
+            expect(firstLastRetrieve).to.be.not.null;
+
             nockObj.get('/liana/v1/permissions').reply(200, permissions2);
 
             new PermissionsChecker('envSecret', 'Users', 'list')
@@ -164,74 +228,15 @@ describe('Service > Permissions', () => {
                 const secondRetrievedPermissions = PermissionsChecker.getPermissions();
                 const secondLastRetrieve = PermissionsChecker.getLastRetrieveTime();
 
-                expect(secondRetrievedPermissions).to.deep.equal(permissions2);
-                expect(secondLastRetrieve.valueOf() - firstLastRetrieve.valueOf() > 0).to.be.true;
+                expect(secondRetrievedPermissions).to.deep.equal(permissions1);
+                expect(secondLastRetrieve.valueOf()).to.be.equal(firstLastRetrieve.valueOf());
 
                 done();
               })
               .catch(done);
-          }, 1200);
-        })
-        .catch(done);
-    });
-
-    it('should not re-retrieve the permissions if it is not expired', (done) => {
-      process.env.PERMISSIONS_EXPIRATION_IN_SECONDS = 1000;
-
-      const intialLastRetrieve = PermissionsChecker.getLastRetrieveTime();
-      const initialRetrievedPermissions = PermissionsChecker.getPermissions();
-
-      expect(intialLastRetrieve).to.be.null;
-      expect(initialRetrievedPermissions).to.be.null;
-
-      const permissions1 = {
-        Users: {
-          permissions: {
-            list: true,
-          },
-        },
-      };
-
-      const permissions2 = {
-        Users: {
-          permissions: {
-            list: true,
-          },
-        },
-        Posts: {
-          permissions: {
-            list: false,
-          }
-        }
-      };
-
-      nockObj.get('/liana/v1/permissions').reply(200, permissions1);
-
-      new PermissionsChecker('envSecret', 'Users', 'list')
-        .perform()
-        .then(() => {
-          const firestRetrievedPermissions = PermissionsChecker.getPermissions();
-          const firstLastRetrieve = PermissionsChecker.getLastRetrieveTime();
-
-          expect(firestRetrievedPermissions).to.deep.equal(permissions1);
-          expect(firstLastRetrieve).to.be.not.null;
-
-          nockObj.get('/liana/v1/permissions').reply(200, permissions2);
-
-          new PermissionsChecker('envSecret', 'Users', 'list')
-            .perform()
-            .then(() => {
-              const secondRetrievedPermissions = PermissionsChecker.getPermissions();
-              const secondLastRetrieve = PermissionsChecker.getLastRetrieveTime();
-
-              expect(secondRetrievedPermissions).to.deep.equal(permissions1);
-              expect(secondLastRetrieve.valueOf()).to.be.equal(firstLastRetrieve.valueOf());
-
-              done();
-            })
-            .catch(done);
-        })
-        .catch(done);
+          })
+          .catch(done);
+      });
     });
   });
 });
