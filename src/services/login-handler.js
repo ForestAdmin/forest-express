c92fe0078785ecbe9cfdb22dd3c68305e39d4fd6
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-08-21 16:19:59 +0200

    [-] Code Style - Capitalize a constant variable

--- src/services/login-handler.js
+++ src/services/login-handler.js
@@ -19,14 +19,14 @@ function LoginHandler({
   const { forestToken, email, password } = authData;
 
   function getTwoFactorResponse(user) {
-    const twoFactorSecretSalt = process.env.FOREST_2FA_SECRET_SALT;
+    const TWO_FACTOR_SECRET_SALT = process.env.FOREST_2FA_SECRET_SALT;
 
-    if (twoFactorSecretSalt === undefined) {
+    if (TWO_FACTOR_SECRET_SALT === undefined) {
       logger.error('Cannot use the two factor authentication because the FOREST_2FA_SECRET_SALT environment variable is not set.');
       throw new Error();
     }
 
-    if (twoFactorSecretSalt.length !== 20) {
+    if (TWO_FACTOR_SECRET_SALT.length !== 20) {
       logger.error('The FOREST_2FA_SECRET_SALT environment variable must be 20 characters long.');
       logger.error('You can generate it using this command: `$ openssl rand -hex 10`');
       throw new Error();
@@ -36,7 +36,7 @@ function LoginHandler({
       return { twoFactorAuthenticationEnabled: true };
     } else {
       const twoFactorAuthenticationSecret = user.two_factor_authentication_secret;
-      const userSecret = new UserSecretCreator(twoFactorAuthenticationSecret, twoFactorSecretSalt)
+      const userSecret = new UserSecretCreator(twoFactorAuthenticationSecret, TWO_FACTOR_SECRET_SALT)
         .perform();
 
       return {
