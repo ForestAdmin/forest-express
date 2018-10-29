a71050909d3bafc0ea84e579fbb518d983ccb75b
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-10-29 10:55:30 +0100

    [-] Code Style - Apply ESLint rules

--- src/deserializers/resource.js
+++ src/deserializers/resource.js
@@ -3,48 +3,46 @@ const P = require('bluebird');
 const logger = require('../services/logger');
 const Schemas = require('../generators/schemas');
 
-function ResourceDeserializer(Implementation, model, params,
-  withRelationships, opts) {
+function ResourceDeserializer(Implementation, model, params, withRelationships, opts) {
   if (!opts) { opts = {}; }
-  var schema = Schemas.schemas[Implementation.getModelName(model)];
+  const schema = Schemas.schemas[Implementation.getModelName(model)];
 
   function extractAttributes() {
-    var attributes = params.data.attributes;
+    let { attributes } = params.data;
     if (params.data.attributes) {
       attributes[schema.idField] = params.data.attributes[schema.idField] ||
         params.data.id;
     }
 
     // NOTICE: Look for some Smart Field setters and apply them if any.
-    var smartFields = _.filter(schema.fields, function (field) {
+    const smartFields = _.filter(schema.fields, (field) => {
       if (field.isVirtual && field.set && field.reference) {
-        logger.warn('The "' + field.field + '" Smart Relationship cannot be updated implementing a "set" function.');
+        logger.warn(`The "${field.field}" Smart Relationship cannot be updated implementing a "set" function.`);
       }
       return field.isVirtual && field.set && !field.reference;
     });
 
-    _.each(schema.fields, function (field) {
+    _.each(schema.fields, (field) => {
       if (field.type === 'Point' && params.data.attributes[field.field]) {
-        var coordinates = params.data.attributes[field.field].split(',');
+        const coordinates = params.data.attributes[field.field].split(',');
         params.data.attributes[field.field] = {
           type: 'Point',
-          coordinates: coordinates
+          coordinates,
         };
       }
     });
 
     return P
-      .each(smartFields, function (field) {
+      .each(smartFields, (field) => {
         // WARNING: The Smart Fields setters may override other changes.
         if (field.field in attributes) {
           return field.set(attributes, attributes[field.field]);
         }
+        return null;
       })
-      .then(function () {
+      .then(() => {
         if (opts.omitNullAttributes) {
-          attributes = _.pickBy(attributes, function (value) {
-            return !_.isNull(value);
-          });
+          attributes = _.pickBy(attributes, value => !_.isNull(value));
         }
 
         return attributes || {};
@@ -52,13 +50,12 @@ function ResourceDeserializer(Implementation, model, params,
   }
 
   function extractRelationships() {
-    return new P(function (resolve) {
-      var relationships = {};
+    return new P((resolve) => {
+      const relationships = {};
 
-      _.each(schema.fields, function (field) {
+      _.each(schema.fields, (field) => {
         if (field.reference && params.data.relationships &&
           params.data.relationships[field.field]) {
-
           if (params.data.relationships[field.field].data === null) {
             // Remove the relationships
             relationships[field.field] = null;
@@ -66,14 +63,12 @@ function ResourceDeserializer(Implementation, model, params,
             // Set the relationship
             if (_.isArray(params.data.relationships[field.field].data)) {
               relationships[field.field] = params.data.relationships[field.field]
-                .data.map(function (d) {
-                  return d.id;
-                });
+                .data.map(d => d.id);
             } else {
               relationships[field.field] = params.data.relationships[field.field]
                 .data.id;
             }
-          }  // Else ignore the relationship
+          } // Else ignore the relationship
         }
       });
 
@@ -81,15 +76,12 @@ function ResourceDeserializer(Implementation, model, params,
     });
   }
 
-  this.perform = function () {
+  this.perform = () => {
     if (withRelationships) {
       return P.all([extractAttributes(), extractRelationships()])
-        .spread(function (attributes, relationships) {
-          return _.extend(attributes, relationships);
-        });
-    } else {
-      return extractAttributes();
+        .spread((attributes, relationships) => _.extend(attributes, relationships));
     }
+    return extractAttributes();
   };
 }
 
