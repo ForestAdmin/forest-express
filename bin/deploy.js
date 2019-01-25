0dcc55350cc90141d43b78a592de06c5d5879c91
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-01-25 18:16:24 +0100

    [*] Code Style - Apply ESLint Rules to the deploy script

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,4 +1,3 @@
-'use strict';
 const moment = require('moment');
 const fs = require('fs');
 const simpleGit = require('simple-git')();
@@ -19,28 +18,28 @@ if (process.argv && process.argv[2]) {
 const versionFile = fs.readFileSync('package.json').toString().split('\n');
 let version = versionFile[3].match(/\w*"version": "(.*)",/)[1];
 version = semver.inc(version, numberToIncrement);
-versionFile[3] = '  "version": "' + version + '",';
+versionFile[3] = `  "version": "${version}",`;
 fs.writeFileSync('package.json', versionFile.join('\n'));
 
 // CHANGELOG
 const data = fs.readFileSync('CHANGELOG.md').toString().split('\n');
 const today = moment().format('YYYY-MM-DD');
 
-data.splice(3, 0, '\n## RELEASE ' + version + ' - ' + today);
+data.splice(3, 0, `\n## RELEASE ${version} - ${today}`);
 const text = data.join('\n');
 
 simpleGit
   .checkout(BRANCH_DEVEL)
-  .then(function() { console.log('Starting pull on ' + BRANCH_DEVEL + '...'); })
-  .pull(function(error) { if (error) { console.log(error); } })
-  .then(function() { console.log(BRANCH_DEVEL + ' pull done.'); })
-  .then(function() { fs.writeFileSync('CHANGELOG.md', text); })
+  .then(() => { console.log(`Starting pull on ${BRANCH_DEVEL}...`); })
+  .pull((error) => { if (error) { console.log(error); } })
+  .then(() => { console.log(`${BRANCH_DEVEL} pull done.`); })
+  .then(() => { fs.writeFileSync('CHANGELOG.md', text); })
   .add(['CHANGELOG.md', 'package.json'])
-  .commit('Release ' + version)
+  .commit(`Release ${version}`)
   .push()
   .checkout(BRANCH_MASTER)
-  .then(function() { console.log('Starting pull on ' + BRANCH_MASTER + '...'); })
-  .pull(function(error) { if (error) { console.log(error); } })
-  .then(function() { console.log(BRANCH_MASTER + ' pull done.'); })
+  .then(() => { console.log(`Starting pull on ${BRANCH_MASTER}...`); })
+  .pull((error) => { if (error) { console.log(error); } })
+  .then(() => { console.log(`${BRANCH_MASTER} pull done.`); })
   .mergeFromTo(BRANCH_DEVEL, BRANCH_MASTER)
   .push();
