ee2d95b2f77275b7894ce9d19864a41d2d4c3676
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-01-28 09:18:07 +0100

    [+] Build - Devs can create prereleases

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,47 +5,51 @@ const semver = require('semver');
 
 const BRANCH_MASTER = 'master';
 const BRANCH_DEVEL = 'devel';
+const RELEASE_OPTIONS = ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease'];
 
-let numberToIncrement = 'patch';
-let prerelease;
+let releaseType = 'patch';
+let prereleaseTag;
 
 if (process.argv) {
   if (process.argv[2]) {
     const option = process.argv[2].replace('--', '');
-    if (['major', 'minor', 'patch'].includes(option)) {
-      numberToIncrement = option;
+    if (RELEASE_OPTIONS.includes(option)) {
+      releaseType = option;
     }
   }
   if (process.argv[3]) {
     const option = process.argv[3].replace('--', '');
-    if (option === 'beta') {
-      prerelease = option;
-    }
+    prereleaseTag = option;
   }
 }
 
 // VERSION
 const versionFile = fs.readFileSync('package.json').toString().split('\n');
 let version = versionFile[3].match(/\w*"version": "(.*)",/)[1];
-version = semver.inc(version, numberToIncrement, prerelease);
+version = semver.inc(version, releaseType, prereleaseTag);
+
 versionFile[3] = `  "version": "${version}",`;
-fs.writeFileSync('package.json', versionFile.join('\n'));
+const newVersionFile = versionFile.join('\n');
 
 // CHANGELOG
-const data = fs.readFileSync('CHANGELOG.md').toString().split('\n');
+const changes = fs.readFileSync('CHANGELOG.md').toString().split('\n');
 const today = moment().format('YYYY-MM-DD');
 
-data.splice(3, 0, `\n## RELEASE ${version} - ${today}`);
-const text = data.join('\n');
+changes.splice(3, 0, `\n## RELEASE ${version} - ${today}`);
+const newChanges = changes.join('\n');
 
 simpleGit
   .checkout(BRANCH_DEVEL)
   .then(() => { console.log(`Starting pull on ${BRANCH_DEVEL}...`); })
   .pull((error) => { if (error) { console.log(error); } })
   .then(() => { console.log(`${BRANCH_DEVEL} pull done.`); })
-  .then(() => { fs.writeFileSync('CHANGELOG.md', text); })
+  .then(() => {
+    fs.writeFileSync('package.json', newVersionFile);
+    fs.writeFileSync('CHANGELOG.md', newChanges);
+  })
   .add(['CHANGELOG.md', 'package.json'])
   .commit(`Release ${version}`)
+  .addTag(`v${version}`)
   .push()
   .checkout(BRANCH_MASTER)
   .then(() => { console.log(`Starting pull on ${BRANCH_MASTER}...`); })
@@ -53,5 +57,4 @@ simpleGit
   .then(() => { console.log(`${BRANCH_MASTER} pull done.`); })
   .mergeFromTo(BRANCH_DEVEL, BRANCH_MASTER)
   .push()
-  .addTag(version)
   .checkout(BRANCH_DEVEL);
