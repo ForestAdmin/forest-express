20a558c2fcdfb2de1b80f6631281a7c2a6fb3b3b
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-02-18 08:26:37 +0100

    [-] Code Style - Apply ESLint rules

--- src/routes/actions.js
+++ src/routes/actions.js
@@ -5,34 +5,31 @@ const path = require('../services/path');
 const logger = require('../services/logger');
 const Schemas = require('../generators/schemas');
 
-module.exports = function (app, model, Implementation, integrator, options) {
+module.exports = function actionRoutes(app, model, Implementation, integrator, options) {
   const modelName = Implementation.getModelName(model);
   const schema = Schemas.schemas[modelName];
 
-  function getFormValuesController (action) {
-    return function (request, response) {
-      var values = action.values ? action.values(request.body.data.attributes.values) : {};
+  function getFormValuesController(action) {
+    return (request, response) => {
+      let values = action.values ? action.values(request.body.data.attributes.values) : {};
 
       if (_.isFunction(values.then)) {
         return values
-          .then(function (valuesComputed) {
+          .then((valuesComputed) => {
             values = valuesComputed;
           })
-          .catch(function (error) {
-            logger.error('Cannot send the values of the "' + action.name + '" Smart Actions ' +
-              'because of an unexpected error: ' + error);
+          .catch((error) => {
+            logger.error(`Cannot send the values of the "${action.name}" Smart Actions because of an unexpected error: ${error}`);
             values = {};
           })
-          .finally(function () {
-            return response.status(200).send(values);
-          });
+          .finally(() => response.status(200).send(values));
       }
 
       return response.status(200).send(values);
     };
   }
 
-  this.perform = function () {
+  this.perform = () => {
     _.each(schema.actions, (action) => {
       if (action.values) {
         let route;
