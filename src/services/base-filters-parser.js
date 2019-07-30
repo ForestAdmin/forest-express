7bc187f2b67071f36d64dbaa44ef037839545420
Valentin Lamatte <valentinl@forestadmin.com>
2019-07-30 15:44:13 +0200

    Fix reviewed stuff

--- src/services/base-filters-parser.js
+++ src/services/base-filters-parser.js
@@ -12,19 +12,19 @@ const parseFiltersString = (filtersString) => {
 
 // NOTICE: Apply the formatCondition function to a condition (leaf).
 const parseCondition = (condition, formatCondition) => {
-  if (_.isEmpty(condition)) throw new InvalidFiltersFormatError('Empty condition in filter');
+  if (_.isEmpty(condition)) { throw new InvalidFiltersFormatError('Empty condition in filter'); }
 
   return formatCondition(condition);
 };
 
 // NOTICE: Call the formatAggregation function on the node and propagate it to its childs or
-//         call the parseCondition function ont the node if the node is a leaf.
+//         call the parseCondition function on the node if the node is a leaf.
 const parseAggregation = (node, formatAggregation, formatCondition) => {
-  if (_.isEmpty(node)) throw new InvalidFiltersFormatError('Empty condition in filter');
-  if (!_.isObject(node)) throw new InvalidFiltersFormatError('Filters cannot be a raw value');
-  if (_.isArray(node)) throw new InvalidFiltersFormatError('Filters cannot be a raw array');
+  if (_.isEmpty(node)) { throw new InvalidFiltersFormatError('Empty condition in filter'); }
+  if (!_.isObject(node)) { throw new InvalidFiltersFormatError('Filters cannot be a raw value'); }
+  if (_.isArray(node)) { throw new InvalidFiltersFormatError('Filters cannot be a raw array'); }
 
-  if (!node.aggregator) return parseCondition(node, formatCondition);
+  if (!node.aggregator) { return parseCondition(node, formatCondition); }
 
   const formatedConditions = [];
 
@@ -43,7 +43,7 @@ const parseAggregation = (node, formatAggregation, formatCondition) => {
 const perform = (filtersString, formatAggregation, formatCondition) => {
   const filters = parseFiltersString(filtersString);
 
-  if (!filters) return null;
+  if (!filters) { return null; }
 
   return parseAggregation(filters, formatAggregation, formatCondition);
 };
@@ -53,8 +53,9 @@ const getConditionAssociation = (condition) => {
   return splittedField.length > 1 ? splittedField[0] : null;
 };
 
-const aggregateAssociations = (_aggregator, conditionsAssociations) =>
-  _.flatten(conditionsAssociations);
+const aggregateAssociations = (_aggregator, conditionsAssociations) => {
+  return _.flatten(conditionsAssociations);
+};
 
 // NOTICE: Recursively populate the associations names from the filters.
 const getAssociations = (filtersString) => {
@@ -63,12 +64,12 @@ const getAssociations = (filtersString) => {
     aggregateAssociations,
     getConditionAssociation,
   );
+  if (!_.isArray(associations)) { return [associations]; }
   return _.uniq(_.compact(associations));
 };
 
-
-exports.perform = perform;
-
-exports.parseFiltersString = parseFiltersString;
-
-exports.getAssociations = getAssociations;
+module.exports = {
+  perform,
+  parseFiltersString,
+  getAssociations,
+};
