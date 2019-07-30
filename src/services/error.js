96a104bad0eb3c667c3e4a33b4497fdbf0b8da4c
Valentin Lamatte <valentinl@forestadmin.com>
2019-07-30 15:46:11 +0200

    exports to module.exports

--- src/services/error.js
+++ src/services/error.js
@@ -1,20 +1,26 @@
-exports.Unauthorized = (message) => {
+const Unauthorized = (message) => {
   this.name = 'Unauthorized';
   this.status = 401;
   this.message = message;
   Error.call(this, message);
 };
 
-exports.UnprocessableEntity = (message) => {
+const UnprocessableEntity = (message) => {
   this.name = 'UnprocessableEntity';
   this.status = 422;
   this.message = message;
   Error.call(this, message);
 };
 
-exports.InvalidFiltersFormatError = (message) => {
+const InvalidFiltersFormatError = (message) => {
   this.name = 'InvalidFiltersFormatError';
   this.message = message || 'The filters format is not a valid JSON string.';
   this.status = 422;
   Error.call(this, message);
+};
+
+module.exports = {
+  Unauthorized,
+  UnprocessableEntity,
+  InvalidFiltersFormatError,
 };
