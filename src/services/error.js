36ed0201fb563fc33ea63d6fb61e9ce6be746def
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-08-02 11:11:29 +0200

    [*] Error Handling - Fix error handling crash

--- src/services/error.js
+++ src/services/error.js
@@ -1,26 +1,20 @@
-const Unauthorized = (message) => {
+exports.Unauthorized = (message) => {
   this.name = 'Unauthorized';
   this.status = 401;
   this.message = message;
   Error.call(this, message);
 };
 
-const UnprocessableEntity = (message) => {
+exports.UnprocessableEntity = (message) => {
   this.name = 'UnprocessableEntity';
   this.status = 422;
   this.message = message;
   Error.call(this, message);
 };
 
-const InvalidFiltersFormatError = (message) => {
+exports.InvalidFiltersFormatError = (message) => {
   this.name = 'InvalidFiltersFormatError';
   this.message = message || 'The filters format is not a valid JSON string.';
   this.status = 422;
   Error.call(this, message);
-};
-
-module.exports = {
-  Unauthorized,
-  UnprocessableEntity,
-  InvalidFiltersFormatError,
 };
