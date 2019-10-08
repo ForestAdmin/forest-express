f1efc856d40ed18bb5004a44766cfcae30ed5083
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-10-08 18:25:23 +0200

    [-] Technical - Apply ESLint rules to an existing code file

--- src/serializers/resource.js
+++ src/serializers/resource.js
@@ -7,25 +7,33 @@ const SmartFieldsValuesInjector = require('../services/smart-fields-values-injec
 const Schemas = require('../generators/schemas');
 const logger = require('../services/logger');
 
-function ResourceSerializer(Implementation, model, records, integrator, opts, meta,
-  fieldsSearched, searchValue, fieldsPerModel) {
-  var modelName = Implementation.getModelName(model);
-  var schema = Schemas.schemas[modelName];
+function ResourceSerializer(
+  Implementation,
+  model,
+  records,
+  integrator,
+  opts,
+  meta,
+  fieldsSearched,
+  searchValue,
+  fieldsPerModel,
+) {
+  const modelName = Implementation.getModelName(model);
+  const schema = Schemas.schemas[modelName];
 
   const needsDateOnlyFormating = Implementation.getLianaName() === 'forest-express-sequelize' &&
     semver.lt(Implementation.getOrmVersion(), '4.0.0');
 
-  var reservedWords = ['meta'];
-  var fieldInfoDateonly = [];
-  var fieldInfoPoint = [];
+  const reservedWords = ['meta'];
+  const fieldInfoDateonly = [];
+  const fieldInfoPoint = [];
 
   function getFieldsNames(fields) {
-    return fields.map(function (field) {
+    return fields.map((field) => {
       if (reservedWords.indexOf(field.field) > -1) {
-        return field.field + ':namespace' + field.field;
-      } else {
-        return field.field;
+        return `${field.field}:namespace${field.field}`;
       }
+      return field.field;
     });
   }
 
@@ -39,11 +47,11 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
     }
   }
 
-  this.perform = function () {
-    var typeForAttributes = {};
+  this.perform = () => {
+    const typeForAttributes = {};
 
     function getAttributesFor(dest, fields) {
-      _.map(fields, function (field) {
+      _.map(fields, (field) => {
         detectFieldWithSpecialFormat(field);
 
         if (field.integration) {
@@ -51,55 +59,49 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
             integrator.defineSerializationOption(model, schema, dest, field);
           }
         } else {
-          var fieldName = field.field;
+          let fieldName = field.field;
           if (reservedWords.indexOf(fieldName) > -1) {
-            fieldName = 'namespace' + fieldName;
+            fieldName = `namespace${fieldName}`;
           }
 
           if (_.isPlainObject(field.type)) {
             dest[fieldName] = {
-              attributes: getFieldsNames(field.type.fields)
+              attributes: getFieldsNames(field.type.fields),
             };
 
             getAttributesFor(dest[field.field], field.type.fields);
           } else if (field.reference) {
-            var referenceType = field.reference.split('.')[0];
-            var referenceSchema = Schemas.schemas[referenceType];
+            const referenceType = field.reference.split('.')[0];
+            const referenceSchema = Schemas.schemas[referenceType];
             typeForAttributes[field.field] = referenceType;
 
             if (!referenceSchema) {
-              logger.error('Cannot find the \'' + referenceType +
-              '\' reference field for \'' + schema.name + '\' collection.');
+              logger.error(`Cannot find the '${referenceType}' reference field for '${schema.name}' collection.`);
               return;
             }
 
-            var fieldReference = referenceSchema.idField;
+            let fieldReference = referenceSchema.idField;
 
             if (_.isArray(field.type) && !fieldReference && referenceSchema.isVirtual) {
-              if (_.find(referenceSchema.fields, function (field) { return field.field === 'id'; })) {
+              if (_.find(referenceSchema.fields, schemaField => schemaField.field === 'id')) {
                 fieldReference = 'id';
               } else {
-                logger.warn('Cannot find the \'idField\' attribute in your \'' +
-                  referenceSchema.name + '\' Smart Collection declaration.');
+                logger.warn(`Cannot find the 'idField' attribute in your '${referenceSchema.name}' Smart Collection declaration.`);
               }
             }
 
-            _.each(referenceSchema.fields, function (field) {
-              detectFieldWithSpecialFormat(field, fieldName);
+            _.each(referenceSchema.fields, (schemaField) => {
+              detectFieldWithSpecialFormat(schemaField, fieldName);
             });
 
             dest[fieldName] = {
               ref: fieldReference,
               attributes: getFieldsNames(referenceSchema.fields),
               relationshipLinks: {
-                related: function (dataSet) {
-                  return {
-                    href: '/forest/' + Implementation.getModelName(model) +
-                      '/' + dataSet[schema.idField] + '/relationships/' +
-                      field.field,
-                  };
-                }
-              }
+                related: dataSet => ({
+                  href: `/forest/${Implementation.getModelName(model)}/${dataSet[schema.idField]}/relationships/${field.field}`,
+                }),
+              },
             };
 
             if (_.isArray(field.type)) {
@@ -108,15 +110,14 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
             }
           }
         }
-
       });
     }
 
     function formatFields(record) {
-      var offsetServer = moment().utcOffset() / 60;
+      const offsetServer = moment().utcOffset() / 60;
 
-      _.each(fieldInfoDateonly, function (fieldInfo) {
-        var dateonly;
+      _.each(fieldInfoDateonly, (fieldInfo) => {
+        let dateonly;
         if (fieldInfo.association && record[fieldInfo.association] && fieldInfo.name &&
           record[fieldInfo.association][fieldInfo.name]) {
           dateonly = moment.utc(record[fieldInfo.association][fieldInfo.name])
@@ -129,7 +130,7 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
         }
       });
 
-      _.each(fieldInfoPoint, function (fieldInfo) {
+      _.each(fieldInfoPoint, (fieldInfo) => {
         if (fieldInfo.association && record[fieldInfo.association] && fieldInfo.name &&
           record[fieldInfo.association][fieldInfo.name]) {
           record[fieldInfo.association][fieldInfo.name] =
@@ -141,39 +142,36 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
       });
     }
 
-    var serializationOptions = {
+    const serializationOptions = {
       id: schema.idField,
       attributes: getFieldsNames(schema.fields),
-      keyForAttribute: function (key) { return key; },
-      typeForAttribute: function (attribute) {
-        return typeForAttributes[attribute] || attribute;
-      },
-      meta: meta
+      keyForAttribute: key => key,
+      typeForAttribute: attribute => typeForAttributes[attribute] || attribute,
+      meta,
     };
 
     getAttributesFor(serializationOptions, schema.fields);
 
     // NOTICE: Format Dateonly field types before serialization.
     if (_.isArray(records)) {
-      _.each(records, function (record) {
-        formatFields(record);
-      });
+      _.each(records, (record) => { formatFields(record); });
     } else {
       formatFields(records);
     }
 
-    return new P(function (resolve) {
+    return new P((resolve) => {
       if (_.isArray(records)) {
-        var smartFieldsValuesInjector;
+        let smartFieldsValuesInjector;
         resolve(P
-          .map(records, function (record) {
+          .map(records, (record) => {
             smartFieldsValuesInjector =
               new SmartFieldsValuesInjector(record, modelName, fieldsPerModel);
             return smartFieldsValuesInjector.perform();
           })
-          .then(function(result) {
+          .then((result) => {
             if (fieldsSearched && smartFieldsValuesInjector) {
-              fieldsSearched = fieldsSearched.concat(smartFieldsValuesInjector.getFieldsForHighlightedSearch());
+              fieldsSearched = fieldsSearched
+                .concat(smartFieldsValuesInjector.getFieldsForHighlightedSearch());
             }
             return result;
           }));
@@ -181,22 +179,20 @@ function ResourceSerializer(Implementation, model, records, integrator, opts, me
         resolve(new SmartFieldsValuesInjector(records, modelName, fieldsPerModel).perform());
       }
     })
-      .then(function () {
-        var decorators = null;
+      .then(() => {
+        let decorators = null;
         if (searchValue) {
           decorators = Implementation.RecordsDecorator.decorateForSearch(
             records,
             fieldsSearched,
-            searchValue
+            searchValue,
           );
           if (decorators) {
             serializationOptions.meta = { decorators };
           }
         }
       })
-      .then(function () {
-        return new JSONAPISerializer(schema.name, records, serializationOptions);
-      });
+      .then(() => new JSONAPISerializer(schema.name, records, serializationOptions));
   };
 }
 
