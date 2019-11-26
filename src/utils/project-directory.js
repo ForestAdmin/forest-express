f050b6e409ce9aad40305740003946a4e1b91dee
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-11-26 08:41:01 +0100

    Improve comments format

--- src/utils/project-directory.js
+++ src/utils/project-directory.js
@@ -6,13 +6,13 @@ class ProjectDirectoryUtils {
   }
 
   getAbsolutePath() {
-    // NOTICE : forest-express has not been sym linked
+    // NOTICE: forest-express has not been sym linked.
     if (this.dirname.includes('node_modules')) {
       const splitPaths = this.dirname.split(path.sep);
       const nodeModuleIndex = splitPaths.indexOf('node_modules');
       const subPathsToProject = splitPaths.slice(0, nodeModuleIndex);
 
-      // NOTICE : on POSIX system, empty path created by previous split is skipped by path.join
+      // NOTICE: on POSIX system, empty path created by previous split is skipped by path.join.
       if (!path.isAbsolute(path.join(...subPathsToProject))) {
         return path.join(path.sep, ...subPathsToProject);
       }
@@ -20,7 +20,7 @@ class ProjectDirectoryUtils {
       return path.join(...subPathsToProject);
     }
 
-    // NOTICE : forest-express is sym linked, assuming the process is running on project directory
+    // NOTICE: forest-express is sym linked, assuming the process is running on project directory.
     return process.cwd();
   }
 }
