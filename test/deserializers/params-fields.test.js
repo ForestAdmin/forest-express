685c7a4d69601ef521faa615127ed5f0f257a607
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2019-12-16 16:40:53 +0100

    Update params-fields.test.js

--- test/deserializers/params-fields.test.js
+++ test/deserializers/params-fields.test.js
@@ -3,8 +3,11 @@ const ParamsFieldsDeserializer = require('../../src/deserializers/params-fields'
 describe('deserializers > params-fields', () => {
   it('should split commas separated strings into arrays', () => {
     expect.assertions(1);
+
     const paramsFields = { model: 'field1,field2,field3' };
-    const result = (new ParamsFieldsDeserializer(paramsFields)).perform();
-    expect(result).toStrictEqual({ model: ['field1', 'field2', 'field3'] });
+    const expectedResult = { model: ['field1', 'field2', 'field3'] };
+    const actualResult = (new ParamsFieldsDeserializer(paramsFields)).perform();
+
+    expect(actualResult).toStrictEqual(expectedResult);
   });
 });
