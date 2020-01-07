f4c1982b1fe0d768e233b8ab1a873a5d850449b2
Sliman <smedini@gmail.com>
2020-01-07 17:33:05 +0100

    http 200

--- src/routes/healthcheck.js
+++ src/routes/healthcheck.js
@@ -3,7 +3,7 @@ const path = require('../services/path');
 module.exports = function HealthCheck(app, opts) {
   this.perform = () => {
     app.get(path.generate('healthcheck', opts), (request, response) => {
-      response.status(204).send();
+      response.status(200).send();
     });
   };
 };
