cf4d4773c0b179f906a57c34d8abc0768c888b00
slimane <slimanm@forestadmin.com>
2020-01-07 17:42:10 +0100

    Delete healthcheck.js

--- src/routes/healthcheck.js
+++
@@ -1,9 +0,0 @@
-const path = require('../services/path');
-
-module.exports = function HealthCheck(app, opts) {
-  this.perform = () => {
-    app.get(path.generate('healthcheck', opts), (request, response) => {
-      response.status(200).send();
-    });
-  };
-};
