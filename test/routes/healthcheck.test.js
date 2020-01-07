964b6b47683592289a12049abf26f89f6ff37b53
Sliman <smedini@gmail.com>
2020-01-07 17:33:05 +0100

    http 200

--- test/routes/healthcheck.test.js
+++ test/routes/healthcheck.test.js
@@ -6,14 +6,14 @@ const authSecret = Array(65).join('1');
 
 describe('routes > healthcheck', () => {
   describe('#GET /forest/healthcheck', () => {
-    it('should return 204', async () => {
+    it('should return 200', async () => {
       expect.assertions(1);
       const app = createServer(envSecret, authSecret);
       await new Promise((done) => {
         request(app)
           .get('/forest/healthcheck')
           .end((error, response) => {
-            expect(response.status).toStrictEqual(204);
+            expect(response.status).toStrictEqual(200);
             done();
           });
       });
