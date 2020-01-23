4dafb5271bc59fa86f47251f8c354b3e99394a06
Raphaël Huchet <raphaelht@gmail.com>
2020-01-23 16:03:29 +0100

    chore: wip

--- src/services/exposed/records-getter.js
+++ src/services/exposed/records-getter.js
@@ -1,6 +1,6 @@
 const AbstractRecordService = require('./abstract-records-service');
 const ParamsFieldsDeserializer = require('../../deserializers/params-fields');
-const RecordsCounter = require('./records-counter');
+const Schemas = require('../../generators/schemas');
 
 const BATCH_PAGE_SIZE = 100;
 
@@ -20,6 +20,10 @@ class RecordsGetter extends AbstractRecordService {
   //         and return an ID list. It could be used to handle both "select all" (query)
   //         and "select some" (ids).
   async getIdsFromRequest(params) {
+    const getIdField = () => {
+      const schema = Schemas.schemas[this.Implementation.getModelName(this.model)];
+      return schema.primaryKeys[0];
+    };
     // NOTICE: If it receives a list of ID, return it as is.
     if (params.body
       && params.body.data
@@ -29,11 +33,15 @@ class RecordsGetter extends AbstractRecordService {
     }
 
     // NOTICE: records IDs are returned with a batch.
-    const totalRecords = await new RecordsCounter(this.model).count(params.query);
-    return Array.from({ length: totalRecords / BATCH_PAGE_SIZE })
+    const totalRecords = await new this.Implementation.ResourcesGetter(
+      this.model,
+      this.lianaOptions,
+      params,
+    ).count(params.query);
+    return Array.from({ length: Math.ceil(totalRecords / BATCH_PAGE_SIZE) })
       .reduce(async (accumulator, _, index) => [...await accumulator, ...(await this.getAll(
         { ...params.query, page: { number: `${index + 1}`, size: `${BATCH_PAGE_SIZE}` } },
-      )).map((record) => record.id)], []);
+      )).map((record) => record[getIdField()])], []);
   }
 }
 
--- test/services/exposed/records-getter.test.js
+++ test/services/exposed/records-getter.test.js
@@ -1,4 +1,25 @@
 const RecordsGetter = require('../../../src/services/exposed/records-getter.js');
+const Schemas = require('../../../src/generators/schemas');
+const usersSchema = require('../../fixtures/users-schema.js');
+
+function getMockedRecordsGetter(modelName = null) {
+  const recordsGetter = new RecordsGetter(modelName);
+  recordsGetter.configStore.Implementation = {
+    ResourcesGetter() {
+      return {
+        perform: () => Promise.resolve([[
+          { id: 1, name: 'foo' },
+          { id: 2, name: 'bar' },
+        ], ['id']]),
+        count: () => 2,
+      };
+    },
+    getModelName(name) {
+      return name;
+    },
+  };
+  return recordsGetter;
+}
 
 describe('services > exposed > records-getter', () => {
   describe('getIdsFromRequest', () => {
@@ -8,6 +29,14 @@ describe('services > exposed > records-getter', () => {
       const request = { body: { data: { attributes: { ids: expectedIds } } } };
       const ids = await new RecordsGetter().getIdsFromRequest(request);
       expect(ids).toBe(expectedIds);
+    });
+    it('should return all entries if query is provided', async () => {
+      expect.assertions(1);
+      Schemas.schemas = { users: usersSchema };
+      const expectedIds = [1, 2];
+      const request = { query: {} };
+      const ids = await getMockedRecordsGetter('users').getIdsFromRequest(request);
+      expect(ids).toStrictEqual(expectedIds);
     });
   });
 });
