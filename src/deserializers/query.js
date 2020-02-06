ce8e85f93986e0b673331c6ffc36bdef7b6b21d6
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2020-02-06 15:16:02 +0100

    Update src/deserializers/query.js
    Co-Authored-By: Jeff LADIRAY <ladirayjeff@gmail.com>

--- src/deserializers/query.js
+++ src/deserializers/query.js
@@ -2,7 +2,7 @@ function QueryDeserializer(attributes) {
   this.perform = () => ({
     ...attributes,
     areAllRecordsSelected: attributes.are_all_records_selected,
-    excluded_ids: attributes.excluded_ids,
+    excludedIds: attributes.excluded_ids,
   });
 }
 
