ec6d8a7a00ff7c476fc4a6858ac67f4a5400c926
Raphaël Huchet <raphaelht@gmail.com>
2020-02-17 12:22:25 +0100

    ci: update changelog and commit changes

---
+++ .releaserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+}
