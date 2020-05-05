bc598a68c03a588affcf31f70a8632e6715ec78d
Vincent Molinié <vincent.molinie@epita.fr>
2020-05-05 10:56:34 +0200

    fix(record serializer): harmonize the usage of the RecordSerializer

--- src/services/exposed/record-serializer.js
+++ src/services/exposed/record-serializer.js
@@ -1,6 +1,18 @@
 const AbstractRecordService = require('./abstract-records-service');
 
 class RecordSerializer extends AbstractRecordService {
+  constructor(model) {
+    if (!model) {
+      throw new Error('Missing required parameters `model` passed to constructor');
+    }
+    if (!(model instanceof Object)) {
+      throw new Error('Parameter passed to RecordSerializer constructor should be an object (ex: `{ name: "myModel" }`)');
+    }
+    if (!model.modelName) {
+      model.modelName = model.name;
+    }
+    super(model);
+  }
 }
 
 module.exports = RecordSerializer;
