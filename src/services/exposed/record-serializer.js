177abdb6c44de32fcd7585d640a80e93e3fa60c5
Vincent Molinié <vincentm@forestadmin.com>
2020-05-25 16:07:58 +0200

    chore: update error to be more explicit
    Co-authored-by: Arnaud Besnier <arnaudibesnier@gmail.com>

--- src/services/exposed/record-serializer.js
+++ src/services/exposed/record-serializer.js
@@ -6,7 +6,7 @@ class RecordSerializer extends AbstractRecordService {
       throw new Error('RecordSerializer initialization error: missing first argument "model"');
     }
     if (!(model instanceof Object)) {
-      throw new Error('Parameter passed to RecordSerializer constructor should be an object (ex: `{ name: "myModel" }`)');
+      throw new Error('RecordSerializer initialization error: "model" argument should be an object (ex: `{ name: "myModel" }`)');
     }
     if (!model.modelName) {
       model.modelName = model.name;
