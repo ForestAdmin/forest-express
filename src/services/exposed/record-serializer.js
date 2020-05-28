c5973b30dbbc2e67af5027d7078a16e9b133bf16
Vincent Molinié <vincentm@forestadmin.com>
2020-05-28 10:13:42 +0200

    fix(record serializer): harmonize the usage of the RecordSerializer (#393)

--- src/services/exposed/record-serializer.js
+++ src/services/exposed/record-serializer.js
@@ -1,6 +1,18 @@
 const AbstractRecordService = require('./abstract-records-service');
 
 class RecordSerializer extends AbstractRecordService {
+  constructor(model) {
+    if (!model) {
+      throw new Error('RecordSerializer initialization error: missing first argument "model"');
+    }
+    if (!(model instanceof Object)) {
+      throw new Error('RecordSerializer initialization error: "model" argument should be an object (ex: `{ name: "myModel" }`)');
+    }
+    if (!model.modelName) {
+      model.modelName = model.name;
+    }
+    super(model);
+  }
 }
 
 module.exports = RecordSerializer;
