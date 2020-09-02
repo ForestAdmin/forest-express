cfa21f676b7cd45adf0b11b31199580580cef451
Guillaume Deslandes <guillaumed@forestadmin.com>
2020-09-02 16:03:22 +0200

    fix(integration): prevent 500 error when no Intercom record is found
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- src/integrations/intercom/services/attributes-getter.js
+++ src/integrations/intercom/services/attributes-getter.js
@@ -10,8 +10,13 @@ function AttributesGetter(Implementation, params, opts, mappingValue) {
       const contactQueryResponse = await ContactGetter
         .getContact(intercom, Implementation, mappingValue, params.recordId);
 
-      // NOTICE: No contact find with the given `recordId` or `mappingValue`
-      if (!contactQueryResponse || !contactQueryResponse.body || !contactQueryResponse.body.data) {
+      // NOTICE: No contact found with the given `recordId` or `mappingValue`
+      if (
+        !contactQueryResponse
+        || !contactQueryResponse.body
+        || !contactQueryResponse.body.data
+        || !contactQueryResponse.body.data[0]
+      ) {
         return null;
       }
 
