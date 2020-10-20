5a0192c3dd7fdebc1b877802add1955832468142
Guillaume Gautreau <guillaumeg@forestadmin.com>
2020-10-20 10:31:55 +0200

    chore(authentication): remove a useless import

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const requireAll = require('require-all');
 const context = require('./context');
 const auth = require('./services/auth');
 
-const initContext = require('./context/init');
-
 const ResourcesRoutes = require('./routes/resources');
 const ActionsRoutes = require('./routes/actions');
 const AssociationsRoutes = require('./routes/associations');
