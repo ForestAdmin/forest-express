987687b5f820453b36d3931368b6becea72b479a
Guillaume Gautreau <guillaume+github@ghusse.com>
2020-11-18 11:43:37 +0100

    feat(authentication): identity the client when registering it (#536)

--- src/context/init.js
+++ src/context/init.js
@@ -40,6 +40,8 @@ function initValue(context) {
  *   FOREST_OIDC_CONFIG_EXPIRATION_IN_SECONDS: number;
  *   FOREST_URL: string;
  *   APPLICATION_URL: string;
+ *   FOREST_AUTH_SECRET: string;
+ *   FOREST_ENV_SECRET: string;
  * }} Env
  *
  * @typedef {{
