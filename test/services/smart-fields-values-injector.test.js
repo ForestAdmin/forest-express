0c44ed0fef9d1364210b286a4d4381f8fa524a0f
GuillaumeCisco <guillaumecisco@gmail.com>
2020-12-02 13:16:11 +0100

    test: fix tests

--- test/services/smart-fields-values-injector.test.js
+++ test/services/smart-fields-values-injector.test.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 const _ = require('lodash');
 
 const SmartFieldsValuesInjector = require('../../src/services/smart-fields-values-injector');
@@ -6,6 +7,42 @@ const usersSchema = require('../fixtures/users-schema.js');
 const addressesSchema = require('../fixtures/addresses-schema.js');
 
 describe('services > smart-fields-values-injector', () => {
+  // Mock user sequelize object
+  class UserWithMagicAccessor {
+    constructor(id) {
+      this.id = id;
+      this.dataValues = { // specific to sequelize
+        id,
+      };
+    }
+  }
+  // mock magic accessor on higher level of prototype
+  UserWithMagicAccessor.prototype.hasAddress = () => false;
+
+
+  // Mock address sequelize object
+  class AddressWithMagicAccessor {
+    constructor(id, user) {
+      this.id = id;
+      this.user = user;
+      this.dataValues = { // specific to sequelize
+        id,
+        user,
+      };
+    }
+  }
+  // mock magic accessor on higher level of prototype
+  AddressWithMagicAccessor.prototype.hasUser = () => false;
+
+  let userRecord;
+  let addressRecord;
+
+  // eslint-disable-next-line jest/no-hooks
+  beforeEach(() => {
+    userRecord = new UserWithMagicAccessor(123);
+    addressRecord = new AddressWithMagicAccessor(456, userRecord);
+  });
+
   describe('without Smart Fields', () => {
     it('should not modify the record', async () => {
       expect.assertions(1);
@@ -13,11 +50,10 @@ describe('services > smart-fields-values-injector', () => {
       const usersSchemaWithoutSmartField = _.cloneDeep(usersSchema);
       usersSchemaWithoutSmartField.fields.shift();
       Schemas.schemas = { users: usersSchemaWithoutSmartField };
-      const record = { dataValues: { id: 123 } };
       const fieldsPerModel = { users: ['id'] };
-      const injector = new SmartFieldsValuesInjector(record, 'users', fieldsPerModel);
+      const injector = new SmartFieldsValuesInjector(userRecord, 'users', fieldsPerModel);
       await injector.perform();
-      expect(record).toStrictEqual({ dataValues: { id: 123 } });
+      expect(userRecord).toStrictEqual(userRecord);
     });
   });
 
@@ -25,17 +61,14 @@ describe('services > smart-fields-values-injector', () => {
     it('should inject the Smart Field value in the record', async () => {
       expect.assertions(1);
       Schemas.schemas = { users: usersSchema };
-      const record = { dataValues: { id: 123 } };
       const fieldsPerModel = { users: ['id', 'smart'] };
-      const injector = new SmartFieldsValuesInjector(record, 'users', fieldsPerModel);
+      const injector = new SmartFieldsValuesInjector(userRecord, 'users', fieldsPerModel);
       await injector.perform();
-      expect(record).toStrictEqual({ dataValues: { id: 123 }, smart: { foo: 'bar' } });
+      expect(userRecord.smart).toStrictEqual({ foo: 'bar' });
     });
   });
 
   describe('with a Smart Relationship that reference a collection having a Smart Field', () => {
-    const userRecord = { dataValues: { id: 123 } };
-    const addressRecord = { dataValues: { id: 456, user: userRecord }, user: userRecord };
     const fieldsPerModel = { user: ['smart'], addresses: ['id', 'user', 'smart_user'], smart_user: ['smart'] };
     it('should inject the Smart Relationship reference', async () => {
       expect.assertions(3);
@@ -51,16 +84,7 @@ describe('services > smart-fields-values-injector', () => {
   describe('with a Smart Relationship that reference a collection having a Smart Field whose name is a magic accessor', () => {
     // NOTICE: note the add of the `hasUser`/`hasAddress` function, this is for
     // mocking sequelize magic accessor
-    const userRecord = {
-      dataValues: { id: 123 },
-      hasAddress: () => false,
-    };
 
-    const addressRecord = {
-      dataValues: { id: 456, user: userRecord },
-      user: userRecord,
-      hasUser: () => false,
-    };
     const fieldsPerModel = { addresses: ['id', 'user', 'hasUser', 'smart_user'], user: ['smart', 'hasAddress'], smart_user: ['smart', 'hasAddress'] };
 
     it('should inject the Smart Relationship reference', async () => {
