d1252cd7e3bd08f54bba1696b0ab382b05686c36
Valentin Lamatte <valentinl@forestadmin.com>
2021-04-16 11:53:32 +0200

    fix: remove comment

--- src/services/scope-service.js
+++ src/services/scope-service.js
@@ -15,31 +15,7 @@ class ScopeService {
   async getScopes(renderingId) {
     const queryParams = { renderingId };
 
-    const scopes = await this.forestServerRequester.perform('/liana/scopes', this.environmentSecret, queryParams);
-    console.log('scopes', scopes);
-    return scopes;
-    // return this.forestServerRequester
-    //   .perform('/liana/scopes', this.environmentSecret, queryParams)
-    //   .then((responseBody) => {
-    //     this.isRolesACLActivated = responseBody.meta
-    //       ? responseBody.meta.rolesACLActivated
-    //       : false;
-
-    //     if (!responseBody.data) return null;
-
-    //     // NOTICE: Addtional permissions - live queries, stats parameters
-    //     this._setStatsPermissions(responseBody.stats, { environmentId });
-
-    //     if (renderingOnly) {
-    //       return responseBody.data.renderings
-    //         ? this._setRenderingPermissions(
-    //           renderingId, responseBody.data.renderings[renderingId], { environmentId },
-    //         )
-    //         : null;
-    //     }
-    //     return this._setPermissions(renderingId, responseBody.data, { environmentId });
-    //   })
-    //   .catch((error) => Promise.reject(new this.VError(error, 'Permissions error')));
+    return this.forestServerRequester.perform('/liana/scopes', this.environmentSecret, queryParams);
   }
 }
 
