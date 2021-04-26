87c8c3fa05f11104225e1a72afa356ab73a463f1
Arnaud Besnier <arnaudibesnier@gmail.com>
2021-04-26 11:50:12 +0200

    refactor: reduce some code complexity

--- src/utils/project-directory.js
+++ src/utils/project-directory.js
@@ -5,6 +5,14 @@ class ProjectDirectoryUtils {
     this.dirname = __dirname;
   }
 
+  static ensureAbsolutePath(subPathsToProject) {
+    // NOTICE: on POSIX system, empty path created by previous split is skipped by path.join.
+    if (!path.isAbsolute(path.join(...subPathsToProject))) {
+      return path.join(path.sep, ...subPathsToProject);
+    }
+    return path.join(...subPathsToProject);
+  }
+
   getAbsolutePath() {
     // NOTICE: forest-express has not been sym linked.
     if (this.dirname.includes('node_modules')) {
@@ -12,12 +20,7 @@ class ProjectDirectoryUtils {
       const nodeModuleIndex = splitPaths.indexOf('node_modules');
       const subPathsToProject = splitPaths.slice(0, nodeModuleIndex);
 
-      // NOTICE: on POSIX system, empty path created by previous split is skipped by path.join.
-      if (!path.isAbsolute(path.join(...subPathsToProject))) {
-        return path.join(path.sep, ...subPathsToProject);
-      }
-
-      return path.join(...subPathsToProject);
+      return ProjectDirectoryUtils.ensureAbsolutePath(subPathsToProject);
     }
 
     // NOTICE: forest-express is sym linked, assuming the process is running on project directory.
