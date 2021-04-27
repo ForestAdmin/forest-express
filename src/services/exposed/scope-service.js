303f08510ec7c6e9bd1d1b25262510a9c6d2b6a5
Valentin Lamatte <valentinl@forestadmin.com>
2021-04-27 17:35:23 +0200

    feat: retrieve single scope from collection name

--- src/services/exposed/scope-service.js
+++ src/services/exposed/scope-service.js
@@ -8,6 +8,11 @@ class ScopeService {
 
     return forestServerRequester.perform('/liana/scopes', envSecret, queryParams);
   }
+
+  static async getScope(renderingId, collectionName) {
+    const scopes = await ScopeService.getScopes(renderingId);
+    return scopes[collectionName];
+  }
 }
 
 module.exports = ScopeService;
