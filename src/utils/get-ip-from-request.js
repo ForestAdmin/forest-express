810d0c1e013d9ec3e2af201f1b53bd125eb6af2f
Olessya KOROVINA <olesyak@forestadmin.com>
2021-06-10 10:57:06 +0200

    fix: prevent IP check from failing for the proxies including the port in the headers (#738)

--- src/utils/get-ip-from-request.js
+++ src/utils/get-ip-from-request.js
@@ -1,13 +1,15 @@
 const ipAddr = require('ipaddr.js');
+const ipRegex = require('ip-regex');
 
 function getIpFromRequest(request) {
   /** @type {string} */
   const forwardedAddresses = request.headers['x-forwarded-for'];
+  const parsedIps = forwardedAddresses?.match(ipRegex());
 
-  if (forwardedAddresses) {
+  if (parsedIps?.length) {
     // If the ip chain contains multiple IPs, the last public IP from the chain is the only
     // one we can trust and it corresponds to real IP that contacted our own reverse proxy
-    return forwardedAddresses.split(',')
+    return parsedIps
       .reverse()
       .map((address) => address.trim())
       .find((address) => ipAddr.parse(address).range() !== 'private');
