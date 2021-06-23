a166586e42940d650094134f45e98ccbb9a98c01
Arnaud Moncel <arnaud.moncel43@gmail.com>
2021-06-23 16:53:36 +0200

    test: fix tests

--- test/routes/actions.test.js
+++ test/routes/actions.test.js
@@ -80,7 +80,7 @@ describe('routes > actions', () => {
     expect(app.post).not.toHaveBeenCalled();
   });
 
-  it('should not create a route when actions.values or actions.hooks.* are missing', async () => {
+  it('should not create a route actions.hooks.* are missing', async () => {
     expect.assertions(3);
 
     const schema = { users: { actions: [{}, {}] } };
@@ -93,80 +93,6 @@ describe('routes > actions', () => {
     expect(pathService.generate).not.toHaveBeenCalled();
     expect(pathService.generateForSmartActionCustomEndpoint).not.toHaveBeenCalled();
     expect(app.post).not.toHaveBeenCalled();
-  });
-
-  describe('when actions.values is present', () => {
-    it('should create a route', async () => {
-      expect.assertions(4);
-
-      const schema = { users: { actions: [{ values: jest.fn(() => ({ name: 'Jane' })), name: 'send invoice' }] } };
-      const {
-        actions, pathService, stringUtils, model, implementation, app,
-      } = initContext(schema).inject();
-
-      await actions.perform(app, schema[model.name], model, implementation, {}, {});
-
-      expect(stringUtils.parameterize).toHaveBeenCalledTimes(1);
-      expect(pathService.generate).toHaveBeenCalledTimes(1);
-      expect(app.post).toHaveBeenCalledTimes(1);
-
-      const [path] = app.post.mock.calls[0];
-      expect(path).toBe('actions/send invoice/values');
-    });
-
-    it('should create a valid route callback', async () => {
-      expect.assertions(4);
-
-      const schemas = { users: { actions: [{ values: jest.fn(() => ({ name: 'Jane' })), name: 'send invoice' }] } };
-      const {
-        actions, model, implementation, app,
-      } = initContext(schemas).inject();
-
-      const request = { body: { data: { attributes: { values: { name: 'Jane' } } } } };
-      const send = jest.fn((values) => values);
-      const response = { status: jest.fn(() => ({ send })) };
-
-      await actions.perform(app, schemas[model.name], model, implementation, {}, {});
-
-      const [, , callback] = app.post.mock.calls[0];
-
-      const result = await callback(request, response);
-
-      expect(schemas.users.actions[0].values).toHaveBeenCalledTimes(1);
-      expect(response.status).toHaveBeenCalledTimes(1);
-      expect(send).toHaveBeenCalledTimes(1);
-      expect(result).toStrictEqual({ name: 'Jane' });
-    });
-
-    it('should handle async values function', async () => {
-      expect.assertions(4);
-
-      const schemas = {
-        users: {
-          actions: [
-            { values: jest.fn(() => Promise.resolve(({ name: 'Jane' }))), name: 'send invoice' },
-          ],
-        },
-      };
-      const {
-        actions, model, implementation, app,
-      } = initContext(schemas).inject();
-
-      const request = { body: { data: { attributes: { values: { name: 'Jane' } } } } };
-      const send = jest.fn((values) => values);
-      const response = { status: jest.fn(() => ({ send })) };
-
-      await actions.perform(app, schemas[model.name], model, implementation, {}, {});
-
-      const [, , callback] = app.post.mock.calls[0];
-
-      const result = await callback(request, response);
-
-      expect(schemas.users.actions[0].values).toHaveBeenCalledTimes(1);
-      expect(response.status).toHaveBeenCalledTimes(1);
-      expect(send).toHaveBeenCalledTimes(1);
-      expect(result).toStrictEqual({ name: 'Jane' });
-    });
   });
 
   describe('when action.hooks is present', () => {
