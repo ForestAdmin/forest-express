e3d22b26bea3aa14d1a1d288bcae9a2c2909b96a
Romain Gilliotte <romaing@forestadmin.com>
2021-08-24 17:50:05 +0200

    fix: optimize further by removing the useless count query

--- src/services/exposed/records-getter.js
+++ src/services/exposed/records-getter.js
@@ -1,11 +1,13 @@
 const AbstractRecordService = require('./abstract-records-service');
 const ParamsFieldsDeserializer = require('../../deserializers/params-fields');
 const Schemas = require('../../generators/schemas');
-const IdsFromRequestRetriever = require('../../services/ids-from-request-retriever');
+const QueryDeserializer = require('../../deserializers/query');
 
 class RecordsGetter extends AbstractRecordService {
+  /**
+   * @param extraParams Deprecated. Should be removed for forest-express@10
+   */
   async getAll(extraParams = {}) {
-    // extraParams is used by getIdsFromRequest for record selection on bulk smart actions.
     const params = { ...this.params, ...extraParams };
 
     // Load records
@@ -23,73 +25,77 @@ class RecordsGetter extends AbstractRecordService {
     return records;
   }
 
-  // NOTICE: This function accept either query or ID list params and return an ID list.
-  //         It could be used to handle both "select all" (query) and "select some" (ids).
-  //         It also handles related data.
-  async getIdsFromRequest(params) {
-    const isRelatedData = (attributes) =>
-      attributes.parentCollectionId
-      && attributes.parentCollectionName
-      && attributes.parentAssociationName;
+  /**
+   * Takes a request from a frontend bulk action (bulk smart-action, bulk delete, ...) and
+   * returns the list of ids that should be affected.
+   *
+   * @fixme Cursors should be used instead of offset/limit.
+   *        This will be plainfully slow on big collections.
+   *
+   * @fixme why are we testing for attrs?.allRecords !== true && attrs.ids to detect "all records"
+   *        queries? IMHO this should be only attrs?.allRecords
+   *
+   * @fixme Composite ids are returned separated by a dash "-".
+   *        Not sure why, we are using pipes "|" in forest-express-sequelize but changing it
+   *        would be a breking change.
+   */
+  async getIdsFromRequest(request) {
+    const { getModelName } = this.Implementation;
+    const { primaryKeys } = Schemas.schemas[getModelName(this.model)];
+    const attrs = new QueryDeserializer(request?.body?.data?.attributes ?? {}).perform();
+    const idsExcludedAsString = attrs?.allRecordsIdsExcluded?.map(String);
 
-    const primaryKeysGetter = () => Schemas.schemas[this.Implementation.getModelName(this.model)];
+    // "select all records" is not selected => we are done.
+    if (attrs?.allRecords !== true && attrs.ids) {
+      return attrs.ids;
+    }
 
-    const recordsGetter = async (attributes) => {
-      const { parentCollectionId, parentCollectionName, parentAssociationName } = attributes;
-      if (isRelatedData(attributes)) {
-        const parentModel = this.modelsManager.getModels()[parentCollectionName];
-        const [records] = await new this.Implementation.HasManyGetter(
-          parentModel,
-          this.model,
-          this.lianaOptions,
-          {
-            ...this.params,
-            ...params,
-            ...attributes.allRecordsSubsetQuery,
-            page: attributes.page,
-            recordId: parentCollectionId,
-            associationName: parentAssociationName,
-          },
-          this.user,
-        ).perform();
-        return records;
+    // Otherwise, query database in a loop to retrieve all ids.
+    const ids = [];
+    for (let pageNo = 1, done = false; !done; pageNo += 1) {
+      const loader = this._getLoader(attrs);
+
+      // ... and keep only packed ids.
+      const [records] = await loader.perform(); // eslint-disable-line no-await-in-loop
+      let recordIds = records.map((record) => primaryKeys.map((primaryKey) => record[primaryKey]).join('-'));
+      if (attrs.allRecordsIdsExcluded) {
+        // Ensure that IDs are comparables (avoid ObjectId or Integer issues).
+        recordIds = recordIds.filter((id) => !idsExcludedAsString.includes(String(id)));
       }
 
-      return this.getAll({
-        ...attributes.allRecordsSubsetQuery,
-        page: attributes.page,
-        fields: { [this.model.name]: primaryKeysGetter().primaryKeys.join(',') || '_id' },
-      });
+      ids.push(...recordIds);
+      done = records.length < 1000;
+    }
+
+    return ids;
+  }
+
+  /** @private helper function for getIdsFromRequest */
+  _getLoader(attrs, pageNo) {
+    const { ResourcesGetter, HasManyGetter, getModelName } = this.Implementation;
+    const modelName = getModelName(this.model);
+    const { primaryKeys } = Schemas.schemas[modelName];
+
+    const params = {
+      ...this.params,
+      ...attrs.allRecordsSubsetQuery, // Where does this comes from?
+      restrictFieldsOnRootModel: true,
+      fields: { [modelName]: primaryKeys.join(',') }, // load only ids
+      page: { number: pageNo, size: 1000 }, // max batch size
     };
 
-    const recordsCounter = async (attributes) => {
-      const { parentCollectionId, parentCollectionName, parentAssociationName } = attributes;
-      if (isRelatedData(attributes)) {
-        const parentModel = this.modelsManager.getModels()[parentCollectionName];
-        return new this.Implementation.HasManyGetter(
-          parentModel,
-          this.model,
-          this.lianaOptions,
-          {
-            ...this.params,
-            ...params,
-            recordId: parentCollectionId,
-            associationName: parentAssociationName,
-          },
-          this.user,
-        ).count();
-      }
+    if (attrs.parentCollectionName && attrs.parentCollectionId && attrs.parentAssociationName) {
+      const parentModel = this.modelsManager.getModelByName(attrs.parentCollectionName);
+      const parentParams = {
+        ...params,
+        recordId: attrs.parentCollectionId,
+        associationName: attrs.parentAssociationName,
+      };
 
-      return new this.Implementation.ResourcesGetter(
-        this.model,
-        this.lianaOptions,
-        { ...this.params, allRecordsSubsetQuery: attributes.allRecordsSubsetQuery },
-        this.user,
-      ).count(attributes.allRecordsSubsetQuery);
-    };
+      return new HasManyGetter(parentModel, this.model, this.lianaOptions, parentParams, this.user);
+    }
 
-    return new IdsFromRequestRetriever(recordsGetter, recordsCounter, primaryKeysGetter)
-      .perform(params);
+    return new ResourcesGetter(this.model, this.lianaOptions, params, this.user);
   }
 }
 
