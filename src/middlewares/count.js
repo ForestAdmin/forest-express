c647faa04b1aa38784dfba334fe362a6a64df705
Shohan RAHMAN <shohanr@forestadmin.com>
2022-01-27 15:00:17 +0100

    feat: send error when using middleware not within a count request

--- src/middlewares/count.js
+++ src/middlewares/count.js
@@ -1,9 +1,18 @@
-const deactivateCountMiddleware = (_, response, next) => {
-  response.status(200).send({
-    meta: {
-      count: 'deactivated',
-    },
-  });
+const deactivateCountMiddleware = (request, response, next) => {
+  const { path } = request;
+  const splittedPath = path.split('/');
+
+  if (splittedPath[splittedPath.length - 1] === 'count') {
+    response.status(200).send({
+      meta: {
+        count: 'deactivated',
+      },
+    });
+  } else {
+    response.status(400).send({
+      erorr: 'This middleware can only be used in count routes.',
+    });
+  }
   next();
 };
 
--- test/middlewares/deactivate-count.unit.test.js
+++ test/middlewares/deactivate-count.unit.test.js
@@ -1,22 +1,47 @@
 const deactivateCount = require('../../src/middlewares/count');
 
 describe('middlewares > deactivateCount', () => {
-  it('should return the right metadata', async () => {
-    expect.assertions(2);
+  describe('when on a count route', () => {
+    it('should return the right metadata', async () => {
+      expect.assertions(2);
 
-    const reqMock = { };
-    const statusMock = { send: () => { } };
-    const respMock = { status: () => statusMock };
-    const sendResult = jest.spyOn(statusMock, 'send');
-    const statusResult = jest.spyOn(respMock, 'status').mockImplementation(() => statusMock);
+      const reqMock = { path: 'collection1/count' };
+      const statusMock = { send: () => { } };
+      const respMock = { status: () => statusMock };
+      const nextMock = () => { };
 
-    deactivateCount(reqMock, respMock);
+      const sendResult = jest.spyOn(statusMock, 'send');
+      const statusResult = jest.spyOn(respMock, 'status').mockImplementation(() => statusMock);
 
-    expect(statusResult).toHaveBeenCalledWith(200);
-    expect(sendResult).toHaveBeenCalledWith({
-      meta: {
-        count: 'deactivated',
-      },
+      deactivateCount(reqMock, respMock, nextMock);
+
+      expect(statusResult).toHaveBeenCalledWith(200);
+      expect(sendResult).toHaveBeenCalledWith({
+        meta: {
+          count: 'deactivated',
+        },
+      });
+    });
+  });
+
+  describe('when not on a count route', () => {
+    it('should return an error', async () => {
+      expect.assertions(2);
+
+      const reqMock = { path: 'collection1/:recordId' };
+      const statusMock = { send: () => { } };
+      const respMock = { status: () => statusMock };
+      const nextMock = () => { };
+
+      const sendResult = jest.spyOn(statusMock, 'send');
+      const statusResult = jest.spyOn(respMock, 'status').mockImplementation(() => statusMock);
+
+      deactivateCount(reqMock, respMock, nextMock);
+
+      expect(statusResult).toHaveBeenCalledWith(400);
+      expect(sendResult).toHaveBeenCalledWith({
+        erorr: 'This middleware can only be used in count routes.',
+      });
     });
   });
 });
