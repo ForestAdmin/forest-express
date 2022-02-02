ed9787259d70a58d547c74fbb53ad1074f2a6034
Shohan Rahman <shohan.rahman@epitech.eu>
2022-02-02 14:51:59 +0100

    feat(count): add middleware to deactivate count (#856)

---
+++ src/middlewares/count.js
@@ -0,0 +1,19 @@
+const deactivateCountMiddleware = (request, response, next) => {
+  const { path } = request;
+  const splittedPath = path.split('/');
+
+  if (splittedPath[splittedPath.length - 1] === 'count') {
+    response.status(200).send({
+      meta: {
+        count: 'deactivated',
+      },
+    });
+  } else {
+    response.status(400).send({
+      error: 'The deactiveCount middleware can only be used in count routes.',
+    });
+  }
+  next();
+};
+
+module.exports = deactivateCountMiddleware;
---
+++ test/middlewares/deactivate-count.unit.test.js
@@ -0,0 +1,47 @@
+const deactivateCount = require('../../src/middlewares/count');
+
+describe('middlewares > deactivateCount', () => {
+  describe('when on a count route', () => {
+    it('should return the right metadata', async () => {
+      expect.assertions(2);
+
+      const reqMock = { path: 'collection1/count' };
+      const statusMock = { send: () => { } };
+      const respMock = { status: () => statusMock };
+      const nextMock = () => { };
+
+      const sendResult = jest.spyOn(statusMock, 'send');
+      const statusResult = jest.spyOn(respMock, 'status').mockImplementation(() => statusMock);
+
+      deactivateCount(reqMock, respMock, nextMock);
+
+      expect(statusResult).toHaveBeenCalledWith(200);
+      expect(sendResult).toHaveBeenCalledWith({
+        meta: {
+          count: 'deactivated',
+        },
+      });
+    });
+  });
+
+  describe('when not on a count route', () => {
+    it('should return an error', async () => {
+      expect.assertions(2);
+
+      const reqMock = { path: 'collection1/:recordId' };
+      const statusMock = { send: () => { } };
+      const respMock = { status: () => statusMock };
+      const nextMock = () => { };
+
+      const sendResult = jest.spyOn(statusMock, 'send');
+      const statusResult = jest.spyOn(respMock, 'status').mockImplementation(() => statusMock);
+
+      deactivateCount(reqMock, respMock, nextMock);
+
+      expect(statusResult).toHaveBeenCalledWith(400);
+      expect(sendResult).toHaveBeenCalledWith({
+        error: 'The deactiveCount middleware can only be used in count routes.',
+      });
+    });
+  });
+});
