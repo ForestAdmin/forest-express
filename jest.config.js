9d2c555f2b085c839d2b9bc1af53f9f19419bd5d
Guillaume Gautreau <guillaumeg@forestadmin.com>
2022-10-04 10:38:11 +0200

    chore: fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -160,7 +160,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
+    '**/*.test.[jt]s',
   ],
 
   // An array of regexp pattern strings that are matched against all test paths,
