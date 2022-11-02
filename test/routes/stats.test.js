07c2f94070a522208d1873babad49455b6885ae6
Vincent Molinié <vincentm@forestadmin.com>
2022-11-02 11:17:05 +0100

    feat(chart): add support for context variables used by Workspaces (#952)

--- test/routes/stats.test.js
+++ test/routes/stats.test.js
@@ -1,39 +1,56 @@
 const { init } = require('@forestadmin/context');
 const Stats = require('../../src/routes/stats');
 
+const chartHandler = {
+  getChartWithContextInjected: jest.fn(),
+};
+init((context) => context.addInstance('chartHandler', chartHandler));
+
 describe('routes > stats', () => {
   describe('.get', () => {
     it('should return a value for objective chart', async () => {
-      init((context) => context);
-
       const app = {};
       const model = { name: 'book' };
 
-      class FakeValueStatGetter {
-        perform = jest.fn(async () => ({ value: { countCurrent: 5 } }));
-      }
+      const performMock = jest.fn().mockResolvedValue(({ value: { countCurrent: 5 } }));
+      const FakeValueStatGetter = jest.fn().mockImplementation(() => ({ perform: performMock }));
 
       const Implementation = {
         getModelName: jest.fn(() => 'books'),
         ValueStatGetter: FakeValueStatGetter,
       };
 
+      const chart = { type: 'Objective', objective: 1000 };
+      chartHandler.getChartWithContextInjected.mockResolvedValue(chart);
+
       const next = jest.fn();
       const req = {
         params: { recordId: '1' },
         query: { timezone: 'Europe/Paris' },
-        user: { renderingId: 1 },
-        body: { type: 'Objective' },
+        user: { renderingId: 1, id: 10 },
+        body: { type: 'Objective', contextVariables: { test: 'me' } },
       };
       const res = {
         send: jest.fn(),
+      };
+      const expectedParams = {
+        timezone: 'Europe/Paris',
+        contextVariables: { test: 'me' },
+        ...chart,
       };
 
       const subject = new Stats(app, model, Implementation);
       await subject.get(req, res, next);
 
-      const resMockParameters = res.send.mock.calls[0][0];
+      expect(chartHandler.getChartWithContextInjected).toHaveBeenCalledOnceWith({
+        userId: 10,
+        renderingId: 1,
+        chartRequest: req.body,
+      });
+      expect(FakeValueStatGetter).toHaveBeenCalledOnceWith(model, expectedParams, {}, req.user);
+      expect(performMock).toHaveBeenCalledOnceWith();
 
+      const resMockParameters = res.send.mock.calls[0][0];
       expect(resMockParameters.data.attributes.value.value).toBe(5);
     });
   });
