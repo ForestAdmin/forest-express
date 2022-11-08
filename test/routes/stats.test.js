1d088ff981f8ebb2dfcecbf15f933918a9a6a07e
Vincent Molinié <vincentm@forestadmin.com>
2022-11-08 10:54:50 +0100

    feat(chart): add support for context variables inside SQL query (#953)

--- test/routes/stats.test.js
+++ test/routes/stats.test.js
@@ -3,6 +3,7 @@ const Stats = require('../../src/routes/stats');
 
 const chartHandler = {
   getChartWithContextInjected: jest.fn(),
+  getQueryForChart: jest.fn(),
 };
 init((context) => context.addInstance('chartHandler', chartHandler));
 
@@ -52,6 +53,67 @@ describe('routes > stats', () => {
 
       const resMockParameters = res.send.mock.calls[0][0];
       expect(resMockParameters.data.attributes.value.value).toBe(5);
+    });
+  });
+
+  describe('.getWithLiveQuery', () => {
+    it('should return a value for objective chart', async () => {
+      const app = {};
+      const model = { name: 'book' };
+
+      const performMock = jest.fn().mockResolvedValue(([{ value: 5, objective: 10 }]));
+      const FakeQueryStatGetter = jest.fn().mockImplementation(() => ({ perform: performMock }));
+
+      const Implementation = {
+        getModelName: jest.fn(() => 'books'),
+        QueryStatGetter: FakeQueryStatGetter,
+      };
+
+      const chart = { query: 'SELECT something as value', contextVariables: { power: 'fly' } };
+      chartHandler.getQueryForChart.mockResolvedValue(chart);
+
+      const next = jest.fn();
+      const req = {
+        params: { recordId: '1' },
+        query: { timezone: 'Europe/Paris' },
+        user: { renderingId: 1, id: 10 },
+        body: { type: 'Objective', contextVariables: { test: 'me', power: 'fake' } },
+      };
+      const res = {
+        send: jest.fn(),
+      };
+      const expectedParams = {
+        type: 'Objective',
+        ...chart,
+        contextVariables: {
+          ...req.body.contextVariables,
+          ...chart.contextVariables,
+        },
+      };
+
+      const subject = new Stats(app, model, Implementation);
+      await subject.getWithLiveQuery(req, res, next);
+
+      expect(chartHandler.getQueryForChart).toHaveBeenCalledOnceWith({
+        userId: 10,
+        renderingId: 1,
+        chartRequest: req.body,
+      });
+      expect(FakeQueryStatGetter).toHaveBeenCalledOnceWith(expectedParams, {});
+      expect(performMock).toHaveBeenCalledOnceWith();
+
+      expect(res.send).toHaveBeenCalledOnceWith({
+        data: {
+          id: expect.toBeString(),
+          type: 'stats',
+          attributes: {
+            value: {
+              value: 5,
+              objective: 10,
+            },
+          },
+        },
+      });
     });
   });
 });
