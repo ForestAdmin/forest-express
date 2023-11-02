ac99332075b06b71a856dba2cb4e977fb297301c
steveb <bunlon.steve@gmail.com>
2023-11-02 14:38:06 +0100

    fix(serialize): developpers can now specify the fields they want to serialize

--- src/services/exposed/record-serializer.js
+++ src/services/exposed/record-serializer.js
@@ -1,5 +1,6 @@
 const { inject } = require('@forestadmin/context');
 const ResourceSerializer = require('../../serializers/resource');
+const ParamsFieldsDeserializer = require('../../deserializers/params-fields');
 
 class RecordSerializer {
   constructor(model, user, query, { configStore } = inject()) {
@@ -17,6 +18,7 @@ class RecordSerializer {
 
     this.model = model;
     this.configStore = configStore;
+    this.query = query;
   }
 
   serialize(records, meta = null) {
@@ -26,6 +28,9 @@ class RecordSerializer {
       records,
       this.configStore.integrator,
       meta,
+      null,
+      null,
+      this.query?.fields ? new ParamsFieldsDeserializer(this.query.fields).perform() : null,
     ).perform();
   }
 }
